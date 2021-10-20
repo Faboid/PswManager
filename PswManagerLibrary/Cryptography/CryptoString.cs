e762c1808fd2c270e239fb29059f1a6640a1419d
Faboid <fabio.meloni.2000@tiscali.it>
2021-10-20 16:49:29 +0200

    Minor refactor for the CryptoString class: instead of static, it's now an instantiable object.
    Specifics:
    Instead of requiring a password, both Encrypt and Decrypt methods will take it from a private field that gets instantiated through the constructor.

--- PswManagerLibrary/Cryptography/CryptoString.cs
+++ PswManagerLibrary/Cryptography/CryptoString.cs
@@ -10,13 +10,19 @@ namespace PswManagerLibrary.Cryptography {
     public class CryptoString {
         //This class is a refactored version of "A Gazhal"'s answer on https://stackoverflow.com/questions/10168240/encrypting-decrypting-a-string-in-c-sharp
 
-        private static readonly byte[] salt = new byte[] { 0x44, 0x53, 0x33, 0x22, 0x68, 0x73, 0xFF, 0x00, 0x33, 0xFA, 0xAA, 0x6E, 0x12, 0xFC, 0xBC };
+        //todo - change the salt from constant to randomized
+        private readonly byte[] salt = new byte[] { 0x44, 0x53, 0x33, 0x22, 0x68, 0x73, 0xFF, 0x00, 0x33, 0xFA, 0xAA, 0x6E, 0x12, 0xFC, 0xBC };
+        private readonly string key;
 
-        public static string Encrypt(string plainText, string password) {
+        public CryptoString(string password) {
+            key = password;
+        }
+
+        public string Encrypt(string plainText) {
 
             byte[] bites = Encoding.Unicode.GetBytes(plainText);
-
-            using(Aes encryptor = GetAes(password)) {
+            
+            using(Aes encryptor = GetAes()) {
 
                 using(MemoryStream ms = new MemoryStream()) {
                     WriteToStream(ms, encryptor.CreateEncryptor(), bites);
@@ -27,12 +33,12 @@ namespace PswManagerLibrary.Cryptography {
             return plainText;
         }
 
-        public static string Decrypt(string cipherText, string password) {
+        public string Decrypt(string cipherText) {
 
             cipherText = cipherText.Replace(" ", "+");
             byte[] bites = Convert.FromBase64String(cipherText);
 
-            using(Aes encryptor = GetAes(password)) {
+            using(Aes encryptor = GetAes()) {
 
                 using(MemoryStream ms = new MemoryStream()) {
                     WriteToStream(ms, encryptor.CreateDecryptor(), bites);
@@ -43,8 +49,7 @@ namespace PswManagerLibrary.Cryptography {
             return cipherText;
         }
 
-
-        private static Aes GetAes(string key) {
+        private Aes GetAes() {
             Aes aes = Aes.Create();
             Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(key, salt);
             aes.Key = rfc.GetBytes(32);
@@ -61,8 +66,4 @@ namespace PswManagerLibrary.Cryptography {
 
     }
 
-    public enum CryptoType {
-        Encrypt,
-        Decrypt
-    }
 }
