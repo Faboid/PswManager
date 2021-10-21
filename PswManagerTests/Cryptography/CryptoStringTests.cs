06ad0cedb6955db0aa2c99485389f72ea8a1195f
Faboid <fabio.meloni.2000@tiscali.it>
2021-10-21 22:01:04 +0200

    Implemented unit tests project and first working test.
    Specifics:
    Implemented "CryptoStringTests" to make sure the encryption and decryption methods are constant.

---
+++ PswManagerTests/Cryptography/CryptoStringTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using PswManagerLibrary.Cryptography;
+
+namespace PswManagerTests.Cryptography {
+    public class CryptoStringTests {
+
+        [Fact]
+        public void EncryptAndDecryptCorrectly() {
+
+            //arrange
+            string password = "rithwydif£ghg\"ytirhs%";
+            CryptoString crypto = new CryptoString(password);
+            string valueToTest = "test/ng this r@ndom value!";
+
+            //act
+            var encryptedValue = crypto.Encrypt(valueToTest);
+            var decryptedValue = crypto.Decrypt(encryptedValue);
+
+            //assert
+            Assert.NotEqual(encryptedValue, valueToTest);
+            Assert.Equal(decryptedValue, valueToTest);
+
+        }
+
+        [Fact]
+        public void EncryptToCorrectValue() {
+
+            //arrange
+            string password = "rithwy*£^^ghg\"ytirhs%";
+            CryptoString crypto = new CryptoString(password);
+            string valueToTest = "test/ng this r@ndom valu?!";
+
+            string expectedResult = "558JNWxBo76STB6WMoWc/VLp7adpDqNZf+qipe+OYAN8Q1etp52Y2lVCtCChUsfFDWXKjYI10FfWgxzKAzTYZw==";
+
+            //act
+            var encryptedValue = crypto.Encrypt(valueToTest);
+
+            //assert
+            Assert.Equal(expectedResult, encryptedValue);
+
+        }
+
+        [Fact]
+        public void DecryptToCorrectValue() {
+
+            //arrange
+            string password = "rithwy*£^^ghg\"ytirhs%";
+            CryptoString crypto = new CryptoString(password);
+            string valueToTest = "558JNWxBo76STB6WMoWc/VLp7adpDqNZf+qipe+OYAN8Q1etp52Y2lVCtCChUsfFDWXKjYI10FfWgxzKAzTYZw==";
+
+            string expectedResult = "test/ng this r@ndom valu?!";
+
+            //act
+            var encryptedValue = crypto.Decrypt(valueToTest);
+
+            //assert
+            Assert.Equal(expectedResult, encryptedValue);
+
+        }
+
+    }
+}
