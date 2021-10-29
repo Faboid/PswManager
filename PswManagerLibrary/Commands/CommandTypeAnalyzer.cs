241d4438fa044aac6f203fd1e6369ef9f8d73a2f
Faboid <fabio.meloni.2000@tiscali.it>
2021-10-30 00:17:01 +0200

    Started building the base for the commands' hierarchy.
    Specifically:
    
    1: Implemented the command class to keep divided the type of command and its arguments.
    
    2: Created a rudimentary lookup table(it will get later refactored into a cleaner implementation) to convert a word(string) into a "CommandType" enum that specifies what kind of command it is.

---
+++ PswManagerLibrary/Commands/CommandTypeAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PswManagerLibrary.Commands {
+
+    /// <summary>
+    /// Used as a lookup table to convert a simple word string into an enum that represents a specific command.
+    /// </summary>
+    internal class CommandTypeAnalyzer {
+
+        internal static CommandType Get(string command) => command switch {
+            "psw" => CommandType.Psw,
+            _ => throw new NotImplementedException()
+        };
+
+    }
+
+    public enum CommandType {
+        Psw
+    }
+}
