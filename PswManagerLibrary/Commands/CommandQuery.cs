8f1e1790d783d169968b886f35f57e4af1fea95c
Faboid <fabio.meloni.2000@tiscali.it>
2021-10-30 15:26:09 +0200

    Moved the "CommandQuery" class into "Commands" folder.

---
+++ PswManagerLibrary/Commands/CommandQuery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace PswManagerLibrary.Commands {
+
+    /// <summary>
+    /// Takes in commands and analyze them to call their appropriate function.
+    /// </summary>
+    public class CommandQuery {
+
+        public CommandQuery() {
+        }
+
+        public void Start(Command command) {
+            switch(command.MainCommand) {
+                case CommandType.Psw:
+                    throw new NotImplementedException();
+            }
+
+        }
+    }
+}
