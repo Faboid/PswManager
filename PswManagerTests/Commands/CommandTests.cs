6608ea185fdcd2e933ccdc7f078d2ded78652b9b
Faboid <fabio.meloni.2000@tiscali.it>
2021-10-30 15:41:39 +0200

    Implemented tests for the Command and the CommandTypeAnalyzer classes.

---
+++ PswManagerTests/Commands/CommandTests.cs
@@ -0,0 +1,32 @@
+﻿using PswManagerLibrary.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests.Commands {
+    public class CommandTests {
+
+        [Fact]
+        public void RetainPasswordCorrectly() {
+
+            //arrange
+            Command command;
+            string commandType = "psw";
+            string argumentsCommand = " di21gh&& #@   - . Yfa %£$,";
+            string fullcommand = $"{commandType} {argumentsCommand}";
+            CommandType expMainCommand = CommandType.Psw;
+
+            //act
+            command = new Command(fullcommand);
+
+            //assert
+            Assert.Equal(command.MainCommand, expMainCommand);
+            Assert.Equal(string.Join(' ', command.Arguments), argumentsCommand);
+
+        }
+
+    }
+}
---
+++ PswManagerTests/Commands/CommandTypeAnalyzerTests.cs
@@ -0,0 +1,28 @@
+﻿using PswManagerLibrary.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests.Commands {
+    public class CommandTypeAnalyzerTests {
+
+        [Theory]
+        [InlineData("psw", CommandType.Psw)]
+        public void HashTableIsCorrect(string input, CommandType expectedType) {
+
+            //arrange
+            CommandType result;
+
+            //act
+            result = CommandTypeAnalyzer.Get(input);
+
+            //assert
+            Assert.Equal(expectedType, result);
+
+        }
+
+    }
+}
