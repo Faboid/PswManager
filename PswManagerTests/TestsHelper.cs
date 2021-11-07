77e36dba5ab73325bb3a42c3d8de4784a3ec9868
Faboid <fabio.meloni.2000@tiscali.it>
2021-11-07 19:48:49 +0100

    Implemented the base of the TestsHelper class, a class whose purpose is helping testing methods that test CRUD operations.

---
+++ PswManagerTests/TestsHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests {
+
+    public class TestsHelper : IDisposable {
+
+        private TestsPaths paths;
+
+        public TestsHelper() {
+            //get non-existing path to create a folder
+            paths = new TestsPaths();
+            Directory.CreateDirectory(paths.WorkingDirectory);
+            File.Create(paths.AccountsFilePath);
+            File.Create(paths.PasswordsFilePath);
+            File.Create(paths.EmailsFilePath);
+
+            //todo - set the needed classes in public(or protected) class instances
+
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+
+            //delete the created folder and all its contents
+            Directory.Delete(paths.WorkingDirectory, true);
+
+        }
+
+    }
+
+    [CollectionDefinition("TestHelperCollection")]
+    public class TestHelperCollection : ICollectionFixture<TestsHelper> {
+
+    }
+
+}
---
+++ PswManagerTests/TestsPathsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xunit;
+
+namespace PswManagerTests {
+    public class TestsPathsTests {
+    
+        [Fact]
+        public void FoundPathIsNonExistentAndValid() {
+
+            //arrange
+            string path;
+
+            //act
+            path = TestsPaths.GetNonExistentFolderPath();
+
+            //assert
+            Assert.True(Path.IsPathRooted(path));
+            Assert.False(Directory.Exists(path));
+
+        }
+
+    }
+}
