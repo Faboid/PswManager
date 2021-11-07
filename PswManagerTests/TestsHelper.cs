2aa6471ace47f8d993748245e4cf0fdb9efbec94
Faboid <fabio.meloni.2000@tiscali.it>
2021-11-07 19:48:49 +0100

    Implemented the base of the TestsHelper class, a class whose purpose is helping testing methods that test CRUD operations.

---
+++ PswManagerTests/TestsHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using PswManagerLibrary.Commands;
+
+namespace PswManagerTests {
+
+    public class TestsHelper : IDisposable {
+
+        private TestsPaths paths;
+        private CommandQuery query;
+
+        public TestsHelper() {
+            //get non-existing path to create a folder
+            paths = new TestsPaths();
+            Directory.CreateDirectory(paths.WorkingDirectory);
+            File.Create(paths.AccountsFilePath);
+            File.Create(paths.PasswordsFilePath);
+            File.Create(paths.EmailsFilePath);
+
+            //set the needed classes in public(or protected) class instances
+            query = new CommandQuery(paths);
+            query.Start(new Command("psw pswpassword email password"));
+
+        }
+
+        public void Dispose() {
+
+            //delete the created folder and all its contents
+            Directory.Delete(paths.WorkingDirectory, true);
+
+        }
+
+    }
+
+    [CollectionDefinition("TestHelperCollection")]
+    public class TestHelperCollection : ICollectionFixture<TestsHelper> {
+
+    }
+
+}
---
+++ PswManagerTests/TestsPaths.cs
@@ -0,0 +1,37 @@
+﻿using PswManagerLibrary.Global;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerTests {
+    public class TestsPaths : IPaths {
+
+        public TestsPaths() { }
+
+        public string WorkingDirectory = GetNonExistentFolderPath();
+
+        public string PasswordsFilePath => $"{WorkingDirectory}\\Passwords.txt";
+
+        public string AccountsFilePath => $"{WorkingDirectory}\\Accounts.txt";
+
+        public string EmailsFilePath => $"{WorkingDirectory}\\Emails.txt";
+
+        public static string GetNonExistentFolderPath() {
+            string output;
+
+            string basepath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            int curr = 0;
+            do {
+                curr++;
+                output = Path.Combine(basepath, curr.ToString());
+            } while(Directory.Exists(output));
+
+            return output;
+        }
+
+    }
+}
