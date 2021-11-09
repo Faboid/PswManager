c29334e1fcc350a175d07e309270459de6b8752e
Faboid <fabio.meloni.2000@tiscali.it>
2021-11-09 18:34:56 +0100

    Implemented testing environment based on a test "database path".

--- PswManagerTests/TestsHelper.cs
+++ PswManagerTests/TestsHelper.cs
@@ -6,25 +6,41 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using PswManagerLibrary.Commands;
+using PswManagerLibrary.Storage;
 
 namespace PswManagerTests {
 
     public class TestsHelper : IDisposable {
 
-        private TestsPaths paths;
-        private CommandQuery query;
+        public static readonly TestsPaths paths;
+        public static readonly CommandQuery query;
+        public static readonly PasswordManager pswManager;
 
-        public TestsHelper() {
+        static TestsHelper() {
             //get non-existing path to create a folder
             paths = new TestsPaths();
             Directory.CreateDirectory(paths.WorkingDirectory);
-            File.Create(paths.AccountsFilePath);
-            File.Create(paths.PasswordsFilePath);
-            File.Create(paths.EmailsFilePath);
+            File.Create(paths.AccountsFilePath).Close();
+            File.Create(paths.PasswordsFilePath).Close();
+            File.Create(paths.EmailsFilePath).Close();
 
             //set the needed classes in public(or protected) class instances
             query = new CommandQuery(paths);
             query.Start(new Command("psw pswpassword emapassword"));
+
+            pswManager = new PasswordManager(paths, "pswpassword", "emapassword");
+        }
+
+        public static void SetUpDefault() {
+            //resets files to empty
+            File.WriteAllText(paths.AccountsFilePath, "");
+            File.WriteAllText(paths.PasswordsFilePath, "");
+            File.WriteAllText(paths.EmailsFilePath, "");
+
+            //creates three default entries
+            query.Start(new Command("create defaultName1 defaultPassword1 defaultEmail1"));
+            query.Start(new Command("create defaultName2 defaultPassword2 defaultEmail2"));
+            query.Start(new Command("create defaultName3 defaultPassword3 defaultEmail3"));
         }
 
         public void Dispose() {
@@ -38,7 +54,7 @@ namespace PswManagerTests {
 
     [CollectionDefinition("TestHelperCollection")]
     public class TestHelperCollection : ICollectionFixture<TestsHelper> {
-
+        
     }
 
 }
---
+++ PswManagerTests/TestsHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests {
+
+    [Collection("TestHelperCollection")]
+    public class TestsHelperTests {
+
+        [Theory]
+        [InlineData("defaultName1", "defaultPassword1", "defaultEmail1")]
+        [InlineData("defaultName2", "defaultPassword2", "defaultEmail2")]
+        [InlineData("defaultName3", "defaultPassword3", "defaultEmail3")]
+        public void SetUpDefaultCorrectly(string name, string password, string email) {
+
+            //arrange
+            var pswManager = TestsHelper.pswManager;
+            var list = new[] { name, password, email };
+
+            //act
+            TestsHelper.SetUpDefault();
+            var res = pswManager.GetPassword(list[0]);
+
+            //assert
+            Assert.Equal(String.Join(' ', list), res);
+
+        }
+
+    }
+}
