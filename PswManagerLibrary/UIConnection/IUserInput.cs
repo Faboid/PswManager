abf3905d820bf671bea94595dddc7efd9327251a
Faboid <fabio.meloni.2000@tiscali.it>
2021-11-11 18:14:16 +0100

    Introduced "UserInput", a class to get user input from the library's classes.

---
+++ PswManagerLibrary/UIConnection/IUserInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.UIConnection {
+    public interface IUserInput {
+
+        bool YesOrNo(string question);
+
+        void SendMessage(string message);
+
+        string RequestAnswer(string message);
+
+        string RequestAnswer();
+    }
+}
