1053299b9c7e3fcae614d37552289f7f651aa540
Faboid <fabio.meloni.2000@tiscali.it>
2021-11-11 18:21:50 +0100

    Began implementating "Token" through "CommandQuery".

--- PswManagerLibrary/Commands/CommandQuery.cs
+++ PswManagerLibrary/Commands/CommandQuery.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using PswManagerLibrary.Global;
 using PswManagerLibrary.Exceptions;
 using PswManagerLibrary.Storage;
+using PswManagerLibrary.Cryptography;
+using PswManagerLibrary.UIConnection;
 
 namespace PswManagerLibrary.Commands {
 
@@ -13,16 +15,20 @@ namespace PswManagerLibrary.Commands {
     /// </summary>
     public class CommandQuery {
 
+        IUserInput userInput;
         IPasswordManager pssManager;
         IPaths mainPaths;
+        IToken token;
 
-        public CommandQuery(IPaths paths) {
+        public CommandQuery(IPaths paths, IUserInput userInput) {
             mainPaths = paths;
+            this.userInput = userInput;
         }
 
-        public CommandQuery(IPaths paths, IPasswordManager customPasswordManager) {
+        public CommandQuery(IPaths paths, IUserInput userInput, IPasswordManager customPasswordManager) {
             mainPaths = paths;
             pssManager = customPasswordManager;
+            this.userInput = userInput;
         }
 
         public string Start(string command) {
@@ -36,8 +42,41 @@ namespace PswManagerLibrary.Commands {
             //todo - add proper results' returns
             switch(command.MainCommand) {
                 case CommandType.Psw:
-                    pssManager = new PasswordManager(mainPaths, arguments[0], arguments[1]);
-                    return "A new password has been set up successfully.";
+
+                    CryptoString passCryptoString = new CryptoString(arguments[0]);
+                    CryptoString emaCryptoString = new CryptoString(arguments[1]);
+
+                    token = new Token(passCryptoString, emaCryptoString, mainPaths);
+                    var result = token.Get();
+
+                    if(result == null) {
+                        string question = "The tokens are missing or not set up. Do you want to create new tokens?";
+                        
+                        if(userInput.YesOrNo(question)) {
+                            //yes
+
+
+                        } else {
+                            //no
+
+
+                        }
+
+                    }
+
+                    string askTokens = $"The tokens are:{Environment.NewLine} {result.Value.passToken}, {result.Value.emaToken}.{Environment.NewLine} Correct?";
+
+                    if(userInput.YesOrNo(askTokens)) {
+                        //yes
+                        pssManager = new PasswordManager(mainPaths, arguments[0], arguments[1]);
+                        return "A new password has been set up successfully.";
+
+                    } else {
+                        //no
+                        return "The operation has been canceled. Enter your passwords once more to make sure the tokens are correct.";
+
+                    }
+
                 case CommandType.Get:
                     var account = pssManager.GetPassword(arguments[0]);
                     return account;
