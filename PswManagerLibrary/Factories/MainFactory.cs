4e484cc620abb791e3967b3daf3a8487cd9c7104
Faboid <fabio.meloni.2000@tiscali.it>
2021-11-30 20:04:53 +0100

    Creation of a factory class that contains every other factory. More below.
    Important note: this factory is currently unused as I'm debating whether to fully implement it or delete it.

---
+++ PswManagerLibrary/Factories/MainFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.Factories {
+    //todo - decide what to do with this
+    public class MainFactory : IMainFactory {
+
+        readonly ICryptoAccountFactory cryptoAccountFactory;
+        readonly IPasswordManagerFactory passwordManagerFactory;
+
+        public MainFactory(ICryptoAccountFactory cryptoAccountFactory, IPasswordManagerFactory passwordManagerFactory) {
+            this.cryptoAccountFactory = cryptoAccountFactory;
+            this.passwordManagerFactory = passwordManagerFactory;
+        }
+
+        public ICryptoAccountFactory GetCryptoAccountFactory() => cryptoAccountFactory;
+
+        public IPasswordManagerFactory GetPasswordManagerFactory() => passwordManagerFactory;
+    }
+}
