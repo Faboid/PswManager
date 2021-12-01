1d62af2b659e1a0c11659a5956fe8bcf7d632e1e
Faboid <fabio.meloni.2000@tiscali.it>
2021-12-01 23:32:38 +0100

    Added tests to regulare ExceptionsHelper.

---
+++ PswManagerTests/Generic/ExceptionsHelperTests.cs
@@ -0,0 +1,76 @@
+﻿using PswManagerLibrary.Exceptions;
+using PswManagerLibrary.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests.Generic {
+    public class ExceptionsHelperTests {
+    
+        [Fact]
+        public void IfTrue_ExceptionShouldBeThrown() {
+
+            //arrange
+            string message = "message";
+            Exception ex = new InvalidCommandException(message);
+            bool isTrue = true;
+
+            //act
+
+            //assert
+            Assert.Throws<InvalidCommandException>(() => isTrue.IfTrueThrow(ex));
+
+        }
+
+        [Fact]
+        public void IfTrue_ExceptionShouldNotBeThrown() {
+
+            //arrange
+            string message = "message";
+            Exception ex = new InvalidCommandException(message);
+            bool isFalse = false;
+
+            //act
+            var result = Record.Exception(() => isFalse.IfTrueThrow(ex));
+
+            //assert
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+        public void IfFalse_ExceptionShouldBeThrown() {
+
+            //arrange
+            string message = "message";
+            Exception ex = new InvalidCommandException(message);
+            bool isFalse = false;
+
+            //act
+
+            //assert
+            Assert.Throws<InvalidCommandException>(() => isFalse.IfFalseThrow(ex));
+
+        }
+
+        [Fact]
+        public void IfFalse_ExceptionShouldNotBeThrown() {
+
+            //arrange
+            string message = "message";
+            Exception ex = new InvalidCommandException(message);
+            bool isTrue = true;
+
+            //act
+            var result = Record.Exception(() => isTrue.IfFalseThrow(ex));
+
+            //assert
+            Assert.Null(result);
+
+        }
+
+    }
+}
