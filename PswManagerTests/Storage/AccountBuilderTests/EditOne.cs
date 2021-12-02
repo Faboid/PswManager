8b46b8b1b0d8d7e3d8a1b83ae71ceca8260dd39f
Faboid <fabio.meloni.2000@tiscali.it>
2021-12-02 19:10:00 +0100

    Refactored "EditOneCorrectly" test.

--- PswManagerTests/Storage/AccountBuilderTests/EditOne.cs
+++ PswManagerTests/Storage/AccountBuilderTests/EditOne.cs
@@ -15,48 +15,54 @@ namespace PswManagerTests.Storage.AccountBuilderTests {
         public static IEnumerable<object[]> EditOneCorrectlyData() {
             var def = TestsHelper.DefaultValues;
 
-            yield return new string[] {
+            yield return new object[] {
                 def.GetValue(1, DefaultValues.TypeValue.Name), "newName1", "newPassword1", "newEmail1",
-                "newName1 newPassword1 newEmail1"
+                ("newName1", "newPassword1", "newEmail1")
             };
-            yield return new string[] {
+            yield return new object[] {
                 def.GetValue(2, DefaultValues.TypeValue.Name), null, "randompassword2", null,
-                $"{def.GetValue(2, DefaultValues.TypeValue.Name)} randompassword2 {def.GetValue(2, DefaultValues.TypeValue.Email)}"
+                (def.GetValue(2, DefaultValues.TypeValue.Name), "randompassword2", def.GetValue(2, DefaultValues.TypeValue.Email))
             };
-            yield return new string[] {
+            yield return new object[] {
                 def.GetValue(1, DefaultValues.TypeValue.Name), null, null, "updatedEmail1",
-                $"{def.GetValue(1, DefaultValues.TypeValue.Name)} {def.GetValue(1, DefaultValues.TypeValue.Password)} updatedEmail1"
+                (def.GetValue(1, DefaultValues.TypeValue.Name), def.GetValue(1, DefaultValues.TypeValue.Password), "updatedEmail1")
             };
         }
 
-        //todo - fix this mess of a test
         [Theory]
         [MemberData(nameof(EditOneCorrectlyData))]
-        public void EditOneCorrectly(string name, string newName, string newPassword, string newEmail, string expectedstring) {
+        public void EditOneCorrectly(string name, string newName, string newPassword, string newEmail, (string expName, string expPass, string expEma) expected) {
 
             //arrange
             TestsHelper.SetUpDefault();
-            if(newPassword is not null) {
-                newPassword = TestsHelper.CryptoAccount.PassCryptoString.Encrypt(newPassword);
-            }
-            if(newEmail is not null) {
-                newEmail = TestsHelper.CryptoAccount.EmaCryptoString.Encrypt(newEmail);
-            }
-
+            (newPassword, newEmail) = Encrypt(newPassword, newEmail);
             AccountBuilder builder = new AccountBuilder(TestsHelper.Paths);
 
-            var splitstrings = expectedstring.Split(' ');
-            (string name, string password, string email) expected = (splitstrings[0], splitstrings[1], splitstrings[2]);
-            (string name, string password, string email) actual;
-
             //act
-            builder.EditOne(name, newName, newPassword, newEmail);
-            actual = builder.GetOne(newName ?? name);
-            (actual.password, actual.email) = TestsHelper.CryptoAccount.Decrypt(actual.password, actual.email);
+            var actual = GenericAct(builder, name, newName, newPassword, newEmail);
 
             //assert
             Assert.Equal(expected, actual);
 
+        }
+
+        private static (string name, string password, string email) GenericAct(AccountBuilder builder, string name, string newName, string newPassword, string newEmail) {
+            builder.EditOne(name, newName, newPassword, newEmail);
+            var output = builder.GetOne(newName ?? name);
+            (output.password, output.email) = TestsHelper.CryptoAccount.Decrypt(output.password, output.email);
+
+            return output;
+        }
+
+        private static (string password, string email) Encrypt(string pass, string ema) {
+            if(pass is not null) {
+                pass = TestsHelper.CryptoAccount.PassCryptoString.Encrypt(pass);
+            }
+            if(ema is not null) {
+                ema = TestsHelper.CryptoAccount.EmaCryptoString.Encrypt(ema);
+            }
+
+            return (pass, ema);
         }
 
     }
