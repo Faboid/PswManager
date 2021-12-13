40637a0b2deab9e2bd833bbb9378479fb699a2fa
Faboid <fabio.meloni.2000@tiscali.it>
2021-12-13 20:07:28 +0100

    Added GetCommand command and its happy path's test.

---
+++ PswManagerLibrary/RefactoringFolder/Commands/GetCommand.cs
@@ -0,0 +1,39 @@
+﻿using PswManagerLibrary.RefactoringFolder.Commands.Validation;
+using PswManagerLibrary.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.RefactoringFolder.Commands {
+    public class GetCommand : BaseCommand {
+
+        private readonly IPasswordManager pswManager;
+
+        public GetCommand(IPasswordManager pswManager) {
+            this.pswManager = pswManager;
+        }
+
+        public override string GetSyntax() {
+            return "get [name]";
+        }
+
+        protected override IReadOnlyList<(bool condition, string errorMessage)> GetConditions(string[] arguments) {
+
+            ValidationCollection collection = new(arguments);
+            collection.Add(arguments != null, "The arguments cannot be null.");
+            collection.Add((args) => args.Length == 1, "Incorrect arguments number.");
+            collection.Add((args) => pswManager.AccountExist(args[0]) == true, "The given account doesn't exist.");
+            collection.Add((args) => string.IsNullOrWhiteSpace(args[0]) == false, "The name cannot be left empty.");
+
+            return collection.Get();
+        }
+
+        protected override CommandResult RunLogic(string[] arguments) {
+            var value = pswManager.GetPassword(arguments[0]);
+
+            return new CommandResult("The account has been retrieved successfully.", true, value);
+        }
+    }
+}
---
+++ PswManagerTests/RefactoringFolderTests/Commands/GetCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using PswManagerLibrary.RefactoringFolder;
+using PswManagerLibrary.RefactoringFolder.Commands;
+using PswManagerTests.TestsHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests.RefactoringFolderTests.Commands {
+
+    [Collection("TestHelperCollection")]
+    public class GetCommandTests {
+
+        readonly GetCommand getCommand;
+
+        public GetCommandTests() {
+            getCommand = new(TestsHelper.PswManager);
+        }
+
+        [Fact]
+        public void GetCommand_Success() {
+
+            //arrange
+            CommandResult result;
+
+            //act
+            result = getCommand.Run(new string[] { TestsHelper.DefaultValues.GetValue(0, DefaultValues.TypeValue.Name) });
+
+            //assert
+            Assert.Equal(TestsHelper.DefaultValues.values[0], result.QueryReturnValue);
+
+        }
+
+
+    }
+}
