21fb5e27b3315da77def6818cc38e2fe2989f5bc
Faboid <fabio.meloni.2000@tiscali.it>
2021-12-14 18:42:18 +0100

    Added bad paths for the GetCommand tests.

--- PswManagerTests/RefactoringFolderTests/Commands/GetCommandTests.cs
+++ PswManagerTests/RefactoringFolderTests/Commands/GetCommandTests.cs
@@ -20,7 +20,7 @@ namespace PswManagerTests.RefactoringFolderTests.Commands {
         }
 
         [Fact]
-        public void GetCommand_Success() {
+        public void CommandSuccess() {
 
             //arrange
             CommandResult result;
@@ -33,6 +33,70 @@ namespace PswManagerTests.RefactoringFolderTests.Commands {
 
         }
 
+        [Fact]
+        public void CommandFailure_AccountDoesNotExist() {
 
+            //arrange
+            TestsHelper.SetUpDefault();
+            var args = new string[] { "notexistentname" };
+
+            //act & assert
+            Failure_GenericTestType(args);
+
+        }
+
+        public static IEnumerable<object[]> NullArgumentsData() {
+            yield return new object[] { null };
+            yield return new object[] { new string[] { null } };
+        }
+
+        [Theory]
+        [MemberData(nameof(NullArgumentsData))]
+        public void CommandFailure_NullArguments(string[] args) {
+
+            //assert & act
+            Failure_GenericTestType(args);
+
+        }
+
+        public static IEnumerable<object[]> IncorrectNumberArgumentsData() {
+            yield return new object[] { new string[] { "validName", "validEmail@emaildomain.com" } };
+            yield return new object[] { new string[] { "justAName", "thisPassword", "validEmail@emaildomain.com" } };
+            yield return new object[] { Array.Empty<string>() };
+            yield return new object[] { new string[] { "firstName", "secondName", "passwordhere", "validEmail@emaildomain.com" } };
+        }
+
+        [Theory]
+        [MemberData(nameof(IncorrectNumberArgumentsData))]
+        public void CommandFailure_IncorrectNumberArguments(string[] args) {
+
+            //act & assert
+            Failure_GenericTestType(args);
+
+        }
+
+        [Fact]
+        public void CommandFailure_EmptyName() {
+
+            //arrange
+            var args = new string[] { "" };
+
+            //act & assert
+            Failure_GenericTestType(args);
+        }
+
+        private void Failure_GenericTestType(string[] args) {
+            bool valid;
+            CommandResult result;
+
+            //act
+            valid = getCommand.Validate(args).success;
+            result = getCommand.Run(args);
+
+            //assert
+            Assert.False(valid);
+            Assert.False(result.Success);
+            Assert.NotEmpty(result.ErrorMessages);
+        }
     }
 }
