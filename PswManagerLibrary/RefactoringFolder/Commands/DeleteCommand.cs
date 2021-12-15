33781cc152b2f30c41a446dc4bb089cfb85e9550
Faboid <fabio.meloni.2000@tiscali.it>
2021-12-15 18:15:45 +0100

    Implemented untested versions of DeleteCommand and EditCommand.

---
+++ PswManagerLibrary/RefactoringFolder/Commands/DeleteCommand.cs
@@ -0,0 +1,39 @@
+﻿using PswManagerLibrary.RefactoringFolder.Commands.Validation;
+using PswManagerLibrary.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.RefactoringFolder.Commands {
+    public class DeleteCommand : BaseCommand {
+
+        //todo - implement tests for this command
+        private readonly IPasswordManager pswManager;
+
+        public DeleteCommand(IPasswordManager pswManager) {
+            this.pswManager = pswManager;
+        }
+
+        public override string GetSyntax() {
+            return "delete [name]";
+        }
+
+        protected override IReadOnlyList<(bool condition, string errorMessage)> GetConditions(string[] args) {
+            ValidationCollection collection = new ValidationCollection(args);
+            collection.Add(args != null, "The arguments cannot be null.");
+            collection.Add((args) => args.Length == 1, "Incorrect arguments number.");
+            collection.Add((args) => pswManager.AccountExist(args[0]) == true, "The given account doesn't exist.");
+            collection.Add((args) => string.IsNullOrWhiteSpace(args[0]) == false, "The name cannot be left empty.");
+
+            return collection.Get();
+        }
+
+        protected override CommandResult RunLogic(string[] arguments) {
+            pswManager.DeletePassword(arguments[0]);
+
+            return new CommandResult("Account deleted successfully.", true);
+        }
+    }
+}
---
+++ PswManagerLibrary/RefactoringFolder/Commands/EditCommand.cs
@@ -0,0 +1,39 @@
+﻿using PswManagerLibrary.RefactoringFolder.Commands.Validation;
+using PswManagerLibrary.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.RefactoringFolder.Commands {
+    public class EditCommand : BaseCommand {
+        //todo - implement tests for this command
+        private readonly IPasswordManager pswManager;
+
+        public EditCommand(IPasswordManager pswManager) {
+            this.pswManager = pswManager;
+        }
+
+        public override string GetSyntax() {
+            return "edit [name] name:[new name]? password:[new password]? email:[new email]?";
+        }
+
+        protected override IReadOnlyList<(bool condition, string errorMessage)> GetConditions(string[] args) {
+            ValidationCollection collection = new ValidationCollection(args);
+            collection.Add(args != null, "The arguments cannot be null.");
+            collection.Add((args) => args.Length >= 2 && args.Length <= 4, "Incorrect arguments number.");
+            collection.Add((args) => pswManager.AccountExist(args[0]) == true, "The given account doesn't exist.");
+            collection.Add((args) => args.All(x => string.IsNullOrWhiteSpace(x) == false), "No value can be left empty.");
+            //todo - add fake email check
+
+            return collection.Get();
+        }
+
+        protected override CommandResult RunLogic(string[] arguments) {
+            pswManager.EditPassword(arguments[0], arguments.Skip(1).ToArray());
+
+            return new CommandResult("The account has been edited successfully.", true);
+        }
+    }
+}
