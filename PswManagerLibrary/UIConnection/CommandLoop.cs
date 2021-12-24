830af6b0338fbafc2048a05a9e94ceb6be3549f5
Faboid <fabio.meloni.2000@tiscali.it>
2021-12-24 20:36:05 +0100

    Added an idea(implemented, but will be refactored) of a UI-detached loop that request commands from the user.

---
+++ PswManagerLibrary/UIConnection/CommandLoop.cs
@@ -0,0 +1,61 @@
+﻿using PswManagerCommands;
+using PswManagerLibrary.Commands;
+using PswManagerLibrary.Commands.NotImplementedYet;
+using PswManagerLibrary.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.UIConnection {
+//todo - this class is doing too much. Split it into multiple classes and use proper DI
+    public class CommandLoop {
+
+        private IUserInput userInput;
+        private CommandQuery query;
+
+        public CommandLoop(IUserInput userInput, IPasswordManager pswManager, IReadOnlyDictionary<string, ICommand> extraCommands = null) {
+            this.userInput = userInput;
+            SetUp(pswManager, extraCommands);
+        }
+
+        private void SetUp(IPasswordManager pswManager, IReadOnlyDictionary<string, ICommand> extraCommands) {
+
+            //set up command query
+            CommandsCollection collection = new CommandsCollection();
+
+            //basic crud commands
+            collection.Add("add", new AddCommand(pswManager));
+            collection.Add("get", new GetCommand(pswManager));
+            collection.Add("edit", new EditCommand(pswManager));
+            collection.Add("delete", new DeleteCommand(pswManager));
+
+            //todo - implement these commands
+            collection.Add("get-all", new GetAllAccountNamesCommand());
+            collection.Add("movedb", new ChangeDatabaseLocationCommand());
+            collection.Add("help", new HelpCommand());
+
+            query = new CommandQuery((IReadOnlyDictionary<string, ICommand>)collection.AsReadOnly().Concat(extraCommands));
+        }
+
+        public void Start() {
+
+            string command;
+            while((command = userInput.RequestAnswer().ToLowerInvariant()) is not "exit") {
+
+                var result = query.Query(command);
+
+                Console.WriteLine(result.BackMessage);
+                if(result.QueryReturnValue != null) {
+                    Console.WriteLine(result.QueryReturnValue);
+                }
+                if(result.Success is false) {
+                    Console.WriteLine(result.GetAllErrorsAsSingleString());
+                }
+            }
+
+        }
+
+    }
+}
