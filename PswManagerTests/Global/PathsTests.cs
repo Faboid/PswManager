be1f59a8af3b6e948288f2751b7e0e62e3d23b69
Faboid <72757782+Faboid@users.noreply.github.com>
2022-01-08 22:42:56 +0100

    Added single test for the Paths class.

---
+++ PswManagerTests/Global/PathsTests.cs
@@ -0,0 +1,58 @@
+﻿using PswManagerLibrary.Global;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests.Global {
+    public class PathsTests : IDisposable {
+
+        static readonly string WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        static readonly string ConfigFilePath = Path.Combine(WorkingDirectory, "Config.txt");
+
+        public PathsTests() {
+            //resets the file in case a past test run didn't dispose correctly
+            if(File.Exists(ConfigFilePath)) {
+                File.Delete(ConfigFilePath);
+            }
+        }
+
+        [Fact]
+        public void CorrectDefaultPaths() {
+
+            //arrange
+            bool configFileExisted;
+            bool configFileExist;
+            string expectedPasswordsFilePath = Path.Combine(WorkingDirectory, "Passwords.txt");
+            string expectedAccountsFilePath = Path.Combine(WorkingDirectory, "Accounts.txt");
+            string expectedEmailsFilePath = Path.Combine(WorkingDirectory, "Emails.txt");
+            string expectedTokenFilePath = Path.Combine(WorkingDirectory, "Token.txt");
+
+            //act
+            configFileExisted = File.Exists(ConfigFilePath);
+            IPaths paths = new Paths();
+            configFileExist = File.Exists(ConfigFilePath);
+
+            //assert
+            Assert.False(configFileExisted);
+            Assert.Equal(WorkingDirectory, Paths.WorkingDirectory);
+            Assert.True(configFileExist);
+
+            Assert.Equal(expectedPasswordsFilePath, paths.PasswordsFilePath);
+            Assert.Equal(expectedAccountsFilePath, paths.AccountsFilePath);
+            Assert.Equal(expectedEmailsFilePath, paths.EmailsFilePath);
+            Assert.Equal(expectedTokenFilePath, paths.TokenFilePath);
+
+        }
+
+        public void Dispose() {
+            if(File.Exists(ConfigFilePath)) {
+                File.Delete(ConfigFilePath);
+            }
+        }
+    }
+}
