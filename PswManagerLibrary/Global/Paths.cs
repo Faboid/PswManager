884d29efd3c40ca4c374b3d6ca2e6d66af02b8c6
Faboid <72757782+Faboid@users.noreply.github.com>
2022-01-09 20:29:16 +0100

    Implemented Paths.MoveMain().

--- PswManagerLibrary/Global/Paths.cs
+++ PswManagerLibrary/Global/Paths.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PswManagerLibrary.Generic;
+using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace PswManagerLibrary.Global {
@@ -19,13 +21,18 @@ namespace PswManagerLibrary.Global {
         public readonly static string WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         private readonly static string ConfigFilePath = Path.Combine(WorkingDirectory, "Config.txt");
 
-        public string PasswordsFilePath => Path.Combine(GetMain(), "Passwords.txt");
+        private const string passwordsFileName = "Passwords.txt";
+        private const string accountsFileName = "Accounts.txt";
+        private const string emailsFileName = "Emails.txt";
+        private const string tokenFileName = "Token.txt";
 
-        public string AccountsFilePath => Path.Combine(GetMain(), "Accounts.txt");
+        public string PasswordsFilePath => Path.Combine(GetMain(), passwordsFileName);
 
-        public string EmailsFilePath => Path.Combine(GetMain(), "Emails.txt");
+        public string AccountsFilePath => Path.Combine(GetMain(), accountsFileName);
 
-        public string TokenFilePath => Path.Combine(GetMain(), "Token.txt");
+        public string EmailsFilePath => Path.Combine(GetMain(), emailsFileName);
+
+        public string TokenFilePath => Path.Combine(GetMain(), tokenFileName);
 
         /// <summary>
         /// Changes the path to the accounts WITHOUT dealing with the current saved data. Any folder and files at the previous path will remain.
@@ -40,7 +47,43 @@ namespace PswManagerLibrary.Global {
         }
 
         public void MoveMain(string path) {
-            throw new NotImplementedException();
+            if(path == GetMain()) {
+                return;
+            }
+
+            if(Directory.Exists(path) == false) {
+                throw new ArgumentException("The given path doesn't point to an existing directory.", nameof(path));
+            }
+
+            string[] pathNames = new string[] { passwordsFileName, accountsFileName, emailsFileName, tokenFileName };
+            string currentMain = GetMain();
+            
+            //checks if there's any existing file among the future new paths
+            if(pathNames.Any(x => File.Exists(Path.Combine(path, x)))) {
+                throw new ArgumentException("The given path is already occupied by same-named files.", nameof(path));
+            }
+
+            try {
+
+                //moves the files to the new folder
+                pathNames.ForEach(x => File.Move(Path.Combine(currentMain, x), Path.Combine(path, x)));
+
+                //sets main to point to the new directory
+                SetMain(path);
+
+            } catch (IOException) {
+
+                //if the operations fail, rollback to the previous version
+                SetMain(currentMain);
+                
+                pathNames.ForEach(x => { 
+                    if(File.Exists(Path.Combine(path, x))) 
+                        File.Move(Path.Combine(path, x), Path.Combine(currentMain, x)); 
+                });
+
+                throw;
+            }
+
         }
 
         private string GetMain() {
