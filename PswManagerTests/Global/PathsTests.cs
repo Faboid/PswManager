417245ecb558233ff29887e2340acc93375e0ca2
Faboid <72757782+Faboid@users.noreply.github.com>
2022-01-09 20:28:21 +0100

    Implemented test for Paths.MoveMain().

--- PswManagerTests/Global/PathsTests.cs
+++ PswManagerTests/Global/PathsTests.cs
@@ -14,12 +14,13 @@ namespace PswManagerTests.Global {
         static readonly string WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         static readonly string TempMainPath = Path.Combine(WorkingDirectory, "ewfsgeweqrqrqrttwqagrthhrjyjewrew"); //gibberish to make sure it's not an existing folder
         static readonly string ConfigFilePath = Path.Combine(WorkingDirectory, "Config.txt");
-        private record FourPaths (string MainPath) {
 
-            public readonly string ExpectedPasswordsFilePath = Path.Combine(MainPath, "Passwords.txt");
-            public readonly string ExpectedAccountsFilePath = Path.Combine(MainPath, "Accounts.txt");
-            public readonly string ExpectedEmailsFilePath = Path.Combine(MainPath, "Emails.txt");
-            public readonly string ExpectedTokenFilePath = Path.Combine(MainPath, "Token.txt");
+        private record FourPaths (string main) {
+
+            public readonly string ExpectedPasswordsFilePath = Path.Combine(main, "Passwords.txt");
+            public readonly string ExpectedAccountsFilePath = Path.Combine(main, "Accounts.txt");
+            public readonly string ExpectedEmailsFilePath = Path.Combine(main, "Emails.txt");
+            public readonly string ExpectedTokenFilePath = Path.Combine(main, "Token.txt");
 
             public void ForEach(Action<string> action) {
                 action.Invoke(ExpectedPasswordsFilePath);
@@ -28,7 +29,7 @@ namespace PswManagerTests.Global {
                 action.Invoke(ExpectedTokenFilePath);
             }
 
-            public void Create() => ForEach(x => File.Create(x));
+            public void Create() => ForEach(x => File.Create(x).Dispose());
             public void Delete() => ForEach(x => File.Delete(x));
         }
 
@@ -72,6 +73,28 @@ namespace PswManagerTests.Global {
 
             //assert
             AssertEqualPaths(expectedPaths, paths);
+            Directory.Delete(TempMainPath, true);
+
+        }
+
+        [Fact]
+        public void MoveMain_ShouldMove() {
+
+            //arrange
+            Paths paths = new Paths();
+            FourPaths previousPaths = new(WorkingDirectory);
+            FourPaths expectedPaths = new(TempMainPath);
+
+            //act
+            Directory.CreateDirectory(TempMainPath);
+            previousPaths.Create();
+            paths.MoveMain(TempMainPath);
+
+            //assert
+            AssertEqualPaths(expectedPaths, paths);
+            previousPaths.ForEach(x => Assert.False(File.Exists(x)));
+            expectedPaths.ForEach(x => Assert.True(File.Exists(x)));
+            expectedPaths.Delete();
             Directory.Delete(TempMainPath, true);
 
         }
