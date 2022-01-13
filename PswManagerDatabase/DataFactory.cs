9b5d5d8a26b8f81e2cdac03d5cf45ca72196c1a6
Faboid <72757782+Faboid@users.noreply.github.com>
2022-01-13 12:17:21 +0100

    Implemented DataFactory, a class to manage the creation and division of IDataConnection.

---
+++ PswManagerDatabase/DataFactory.cs
@@ -0,0 +1,30 @@
+﻿using PswManagerDatabase.DataAccess;
+using PswManagerDatabase.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerDatabase {
+    public class DataFactory : IDataFactory {
+
+        private readonly IDataConnection dataConnection;
+
+        public DataFactory() {
+            dataConnection = new DataConnection();
+        }
+
+        public IDataConnection GetDataConnection() => dataConnection;
+
+        public IPathsEditor GetPathsEditor() => dataConnection;
+
+        public IDataCreator GetDataCreator() => dataConnection;
+
+        public IDataDeleter GetDataDeleter() => dataConnection;
+
+        public IDataEditor GetDataEditor() => dataConnection;
+
+        public IDataReader GetDataReader() => dataConnection;
+    }
+}
---
+++ PswManagerDatabase/IDataFactory.cs
@@ -0,0 +1,20 @@
+﻿using PswManagerDatabase.DataAccess;
+using PswManagerDatabase.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerDatabase {
+    public interface IDataFactory {
+
+        IDataConnection GetDataConnection();
+        IPathsEditor GetPathsEditor();
+        IDataCreator GetDataCreator();
+        IDataReader GetDataReader();
+        IDataEditor GetDataEditor();
+        IDataDeleter GetDataDeleter();
+
+    }
+}
