9011fda7b479cd5403707c52fd6cdb36bebd5772
Faboid <72757782+Faboid@users.noreply.github.com>
2022-01-14 19:17:49 +0100

    Added AccountModel constructor.

--- PswManagerDatabase/Models/AccountModel.cs
+++ PswManagerDatabase/Models/AccountModel.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace PswManagerDatabase.Models {
     public class AccountModel {
 
+        public AccountModel() {
+
+        }
+
+        public AccountModel(string name, string password, string email) {
+            Name = name;
+            Password = password;
+            Email = email;
+        }
+
         public string Name { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
