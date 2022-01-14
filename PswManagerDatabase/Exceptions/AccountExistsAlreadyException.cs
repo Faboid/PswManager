cdfb48feb619c8ab4c09e1e1e84b671e38aaec4d
Faboid <72757782+Faboid@users.noreply.github.com>
2022-01-14 19:18:22 +0100

    Imported account existence-related exceptions from the main library.

---
+++ PswManagerDatabase/Exceptions/AccountExistsAlreadyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PswManagerDatabase.Exceptions {
+    public class AccountExistsAlreadyException : Exception {
+        public AccountExistsAlreadyException() {
+        }
+
+        public AccountExistsAlreadyException(string message) : base(message) {
+        }
+
+        public AccountExistsAlreadyException(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected AccountExistsAlreadyException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
---
+++ PswManagerDatabase/Exceptions/InexistentAccountException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PswManagerDatabase.Exceptions {
+    public class InexistentAccountException : Exception {
+        public InexistentAccountException() { }
+
+        public InexistentAccountException(string message) : base(message) { }
+
+        public InexistentAccountException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected InexistentAccountException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
