df6bb87c8ce3e1fdb8b782f6bd3313c74b7f96ec
Faboid <72757782+Faboid@users.noreply.github.com>
2022-01-15 20:29:15 +0100

    Implemented ConnectionResult, a model class to send a database call results back to the caller.

---
+++ PswManagerDatabase/Models/ConnectionResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerDatabase.Models {
+    public class ConnectionResult {
+
+        public ConnectionResult(bool success, string errorMessage = null) {
+            Success = success;
+            ErrorMessage = errorMessage;
+        }
+
+        public bool Success { get; init; }
+        public string ErrorMessage { get; init; }
+
+    }
+
+    public class ConnectionResult<TValue> : ConnectionResult where TValue : class {
+        public ConnectionResult(bool success, string errorMessage = null) : base(success, errorMessage) {
+        }
+
+        public ConnectionResult(bool success, string errorMessage = null, TValue value = null) : base(success, errorMessage) {
+            Value = value;
+        }
+
+        public ConnectionResult(bool success, TValue value = null) : base(success) {
+            Value = value;
+        }
+
+
+        public TValue Value { get; init; }
+
+    }
+
+}
