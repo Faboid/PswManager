9e214a19a18cc1b91a5dbfa02d6bf1187c629752
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-03 23:08:05 +0100

    Implemented a test to validate whether values that have [Required] attribute are null or empty. [Untested]

---
+++ PswManagerCommands/Validation/AutoValidation.cs
@@ -0,0 +1,40 @@
+﻿using PswManagerCommands.Validation.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerCommands.Validation {
+    public class AutoValidation<T> {
+
+        readonly List<string> errors = new();
+        readonly IReadOnlyList<PropertyInfo> properties = typeof(T).GetProperties();
+        readonly IReadOnlyList<PropertyInfo> requiredProperties;
+
+        public AutoValidation() {
+            requiredProperties = properties.Where(x => x.GetCustomAttribute<RequiredAttribute>() != null).ToList();
+        }
+
+        public IReadOnlyList<string> GetErrors() {
+            return errors;
+        }
+
+        public void Validate(T obj) {
+            RequiredPropertiesHaveValues(obj);
+        }
+
+        private void RequiredPropertiesHaveValues(T obj) {
+
+            //check they're not empty
+            var emptyProps = requiredProperties.Where(x => string.IsNullOrEmpty((string)x.GetValue(obj)));
+
+            //add error to list
+            foreach(var s in emptyProps) {
+                errors.Add($"You must provide a value for {s.Name}.");
+            }
+        }
+
+    }
+}
