651223cee305f11400f916161ec394426095ffc6
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-07 20:09:11 +0100

    Implemented attribute-validationLogic pair. (untested)

---
+++ PswManagerLibrary/Commands/Validation/Attributes/VerifyAccountExistenceAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.Commands.Validation.Attributes {
+    [AttributeUsage(AttributeTargets.Property)]
+    public class VerifyAccountExistenceAttribute : Attribute {
+
+        public bool ShouldExist;
+
+        public VerifyAccountExistenceAttribute(bool shouldExist) { 
+            ShouldExist = shouldExist;
+        }
+
+    }
+}
---
+++ PswManagerLibrary/Commands/Validation/ValidationTypes/VerifyAccountExistenceLogic.cs
@@ -0,0 +1,33 @@
+﻿using PswManagerCommands.Validation.Models;
+using PswManagerDatabase.DataAccess.Interfaces;
+using PswManagerLibrary.Commands.Validation.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.Commands.Validation.ValidationLogic {
+    public class VerifyAccountExistenceLogic : ValidationLogic<VerifyAccountExistenceAttribute, string> {
+
+        private readonly IDataHelper dataHelper;
+
+        public VerifyAccountExistenceLogic(IDataHelper dataHelper) {
+            this.dataHelper = dataHelper;
+        }
+
+        public override bool Validate(VerifyAccountExistenceAttribute attribute, string value, out string errorMessage) {
+            errorMessage = string.Empty;
+            bool valid = dataHelper.AccountExist(value) == attribute.ShouldExist;
+
+            if(!valid) {
+                errorMessage = attribute.ShouldExist switch {
+                    true => "The given account doesn't exist.",
+                    false => "The account exists already."
+                };
+            }
+
+            return valid;
+        }
+    }
+}
