a6a8b0bb0a4d8b1400d929b7e398312f47575ef3
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-14 11:31:58 +0100

    Implemented happy path for Requester's unit test.

---
+++ PswManagerTests/Parsing/RequesterTests.cs
@@ -0,0 +1,85 @@
+﻿using PswManagerLibrary.InputBuilder;
+using PswManagerLibrary.InputBuilder.Attributes;
+using PswManagerLibrary.UIConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests.Parsing {
+    public class RequesterTests {
+
+        [Fact]
+        public void Success() {
+
+            //arrange
+            string name = "Yehallo";
+            string password = "rugtyrh&&ieow";
+            //string email = "email@here.com";
+            List<string> answers = new() { name, password };
+            List<bool> yesOrNoAns = new() { true };
+            FakeUserInput fakeUserInput = new(answers, yesOrNoAns);
+            Requester requester = new(typeof(InputObject), fakeUserInput);
+
+            //act
+            var success = requester.Build(out var obj);
+
+            //assert
+            Assert.True(success);
+            Assert.True(obj is InputObject);
+            var output = (InputObject)obj;
+            Assert.Equal(name, output.Name);
+            Assert.Equal(password, output.Password);
+            Assert.Equal(default, output.Email);
+
+        }
+
+    }
+
+    internal class InputObject {
+
+        [Request("Input the account's name.")]
+        public string Name { get; set; }
+
+        [Request("Input the password.")]
+        public string Password { get; set; }
+
+        [Request("Input the email.", true)]
+        public string Email { get; set; }
+    }
+
+    internal class FakeUserInput : IUserInput {
+
+        public FakeUserInput(List<string> answers, List<bool> yesOrNoAnswers) {
+            this.answers = answers;
+            this.yesOrNoAnswers = yesOrNoAnswers;
+        }
+
+        private readonly List<string> answers;
+        private readonly List<bool> yesOrNoAnswers;
+
+        public string RequestAnswer(string message) {
+            var ans = answers.FirstOrDefault();
+            answers.Remove(ans);
+            return ans;
+        }
+
+        public string RequestAnswer() {
+            var ans = answers.FirstOrDefault();
+            answers.Remove(ans);
+            return ans;
+        }
+
+        public void SendMessage(string message) {
+            
+        }
+
+        public bool YesOrNo(string question) {
+            var ans = yesOrNoAnswers.First();
+            yesOrNoAnswers.Remove(ans);
+            return ans;
+        }
+    }
+}
