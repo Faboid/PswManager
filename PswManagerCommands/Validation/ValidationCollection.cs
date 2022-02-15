ecfbd5767b521d31524b1b84852a60666e279dd0
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-15 23:18:52 +0100

    Implemented a generic version of ValidationCollection & its interface.

--- PswManagerCommands/Validation/ValidationCollection.cs
+++ PswManagerCommands/Validation/ValidationCollection.cs
@@ -95,4 +95,41 @@ namespace PswManagerCommands.Validation {
                 => validatorsDictionary.TryGetValue(x, out (bool valid, string _) result) && result.valid);
         }
     }
+
+
+    internal class ValidationCollection<T> : IValidationCollection<T> {
+
+        private readonly Dictionary<int, (bool, string)> validatorsDictionary = new();
+        private readonly T obj;
+
+        public ValidationCollection(T input) {
+            obj = input;
+        }
+
+        public IReadOnlyList<(bool condition, string errorMessage)> GetResult() {
+            return validatorsDictionary.Select(x => x.Value).ToList();
+        }
+
+        public T GetObject() {
+            return obj;
+        }
+
+        public void Add(ushort index, bool condition, string errorMessage) {
+            validatorsDictionary.Add(index, (condition, errorMessage));
+        }
+
+        public void Add(IndexHelper index, Func<T, bool> conditionFunction, string errorMessage) {
+            if(!IndexesAreValid(index.RequiredSuccesses)) {
+                return;
+            }
+
+            validatorsDictionary.Add(index.Index, (conditionFunction.Invoke(obj), errorMessage));
+        }
+
+        public bool IndexesAreValid(params int[] indexes) {
+            return indexes.All(x
+                => validatorsDictionary.TryGetValue(x, out (bool valid, string _) result) && result.valid);
+        }
+
+    }
 }
