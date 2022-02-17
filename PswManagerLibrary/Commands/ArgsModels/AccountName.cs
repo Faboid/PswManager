4b9f9a12f53b3ca4b63601ee97f9e372ee15e3df
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-17 23:32:49 +0100

    Added object for the DeleteCommand arguments.

---
+++ PswManagerLibrary/Commands/ArgsModels/AccountName.cs
@@ -0,0 +1,15 @@
+﻿using PswManagerLibrary.InputBuilder.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerLibrary.Commands.ArgsModels {
+    public class AccountName {
+
+        [Request("Name", "Insert the name of the account you wish to delete.")]
+        public string Name { get; set; }
+
+    }
+}
