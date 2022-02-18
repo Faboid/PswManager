0419bda64c6177f7846eecb83ae54438e3f7eedf
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-18 22:34:00 +0100

    Migrated EditCommand's parent to the new BaseCommand<T>. Specifics below:
    1. Deleted all now-unnecessary syntax logic from EditCommand.
    2. Rewrote the EditCommandTests to make use of EditCommand's input object.

--- PswManagerLibrary/Commands/EditCommand.cs
+++ PswManagerLibrary/Commands/EditCommand.cs
@@ -1,23 +1,17 @@
 ﻿using PswManagerCommands;
-using PswManagerCommands.AbstractCommands;
+using PswManagerCommands.TempLocation;
 using PswManagerCommands.Validation;
 using PswManagerDatabase.DataAccess.Interfaces;
 using PswManagerDatabase.Models;
-using PswManagerHelperMethods;
+using PswManagerLibrary.Commands.AutoCommands.ArgsModels;
 using PswManagerLibrary.Cryptography;
 using PswManagerLibrary.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace PswManagerLibrary.Commands {
-    public sealed class EditCommand : BaseCommand {
+    public sealed class EditCommand : BaseCommand<EditAccountModel> {
 
         private readonly IDataEditor dataEditor;
         private readonly ICryptoAccount cryptoAccount;
-        public const string InvalidSyntaxMessage = "Invalid syntax used for this command. For more informations, run [help edit]";
-        public const string InvalidKeyFound = "Invalid key(s) has been found. Valid keys: name, password, email.";
-        public const string DuplicateKeyFound = "Duplicate keys aren't allowed.";
 
         private record SyntaxCheckResult {
             public bool ValidSyntax { get; set; } = true;
@@ -34,106 +28,29 @@ namespace PswManagerLibrary.Commands {
             return "Edits an existing account with the provided arguments.";
         }
 
-        public override string GetSyntax() {
-            return "edit [name] name:[new name]? password:[new password]? email:[new email]?";
-        }
+        protected override IValidationCollection<EditAccountModel> AddConditions(IValidationCollection<EditAccountModel> collection) {
 
-        protected override IValidationCollection AddConditions(IValidationCollection collection) {
-            collection.AddCommonConditions(2, 4);
-            collection.AddAccountShouldExistCondition(0, dataEditor);
-
-            if(collection.IndexesAreValid(collection.NullIndexCondition, collection.NullOrEmptyArgsIndexCondition, collection.CorrectArgsNumberIndexCondition)) {
-
-                var syntaxCheckResult = CheckSyntax(collection.GetArguments());
-                collection.Add(1, syntaxCheckResult.ValidSyntax, InvalidSyntaxMessage);
-                collection.Add(2, syntaxCheckResult.ValidKeys, InvalidKeyFound);
-                collection.Add(3, syntaxCheckResult.NoDuplicateKeys, DuplicateKeyFound);
-            }
+            //todo - check if at least one of the "new" properties have a value
+            collection.AddAccountShouldExistCondition(0, collection.GetObject().Name, dataEditor);
 
             return collection;
         }
 
-        protected override CommandResult RunLogic(string[] arguments) {
-            string name = arguments[0];
-            AccountModel newValues = new();
-            var keys = arguments
-                .Skip(1)
-                .Select(x => x.Split(':').First().ToLowerInvariant())
-                .ToArray();
-            var values = arguments
-                .Skip(1)
-                .Select(x => x.Split(':').Skip(1))
-                .Select(x => string.Join(':', x))
-                .ToArray();
+        protected override CommandResult RunLogic(EditAccountModel arguments) {
 
-            for(int i = 0; i < keys.Length; i++) {
-                switch(keys[i]) {
-                    case "name":
-                        newValues.Name = values[i];
-                        break;
-                    case "password":
-                        newValues.Password = cryptoAccount.GetPassCryptoString().Encrypt(values[i]);
-                        break;
-                    case "email":
-                            newValues.Email = cryptoAccount.GetEmaCryptoString().Encrypt(values[i]);
-                        break;
-                    default:
-                        return new CommandResult($"Operation failed. The key [{keys[i]}] is invalid.", false);
-                }
+            AccountModel newValues = new();
+            newValues.Name = arguments.NewName;
+            if(!string.IsNullOrWhiteSpace(arguments.NewPassword)) {
+                newValues.Password = cryptoAccount.GetPassCryptoString().Encrypt(arguments.NewPassword);
+            }
+            if(!string.IsNullOrWhiteSpace(arguments.NewEmail)) {
+                newValues.Email = cryptoAccount.GetEmaCryptoString().Encrypt(arguments.NewEmail);
             }
 
-            dataEditor.UpdateAccount(arguments[0], newValues);
+            dataEditor.UpdateAccount(arguments.Name, newValues);
 
             return new CommandResult("The account has been edited successfully.", true);
         }
 
-        private static SyntaxCheckResult CheckSyntax(in string[] args) {
-            var result = new SyntaxCheckResult();
-
-            try {
-                var argsToTest = args.Skip(1); //skips the name
-
-                if(argsToTest.Any(x => !x.Contains(':'))) {
-                    result.ValidSyntax = false;
-                    return result;
-                }
-
-                //string key, bool isUsed
-                Dictionary<string, bool> keys = GetValidKeys();
-
-                var givenKeyArguments = argsToTest.Select(x => x.Split(':').First());
-
-                CheckKeys(ref result, keys, givenKeyArguments);
-            } catch(Exception) {
-                result.ValidSyntax = false;
-            }
-
-            return result;
-        }
-
-        private static void CheckKeys(ref SyntaxCheckResult result, Dictionary<string, bool> keys, IEnumerable<string> givenKeyArguments) {
-
-            foreach(string s in givenKeyArguments) {
-
-                if(keys.ContainsKey(s)) {
-
-                    if(keys[s] == true) {
-                        result.NoDuplicateKeys= false;
-                    }
-                    keys[s] = true;
-
-                } else {
-                    result.ValidKeys = false;
-                }
-            }
-        }
-
-        private static Dictionary<string, bool> GetValidKeys() {
-            Dictionary<string, bool> keys = new();
-            keys.Add("name", false);
-            keys.Add("password", false);
-            keys.Add("email", false);
-            return keys;
-        }
     }
 }
--- PswManagerTests/Commands/EditCommandTests.cs
+++ PswManagerTests/Commands/EditCommandTests.cs
@@ -5,6 +5,7 @@ using PswManagerDatabase.DataAccess.Interfaces;
 using PswManagerLibrary.Commands;
 using PswManagerLibrary.Extensions;
 using PswManagerLibrary.Storage;
+using PswManagerTests.Commands.Helper;
 using PswManagerTests.TestsHelpers;
 using System;
 using System.Collections.Generic;
@@ -25,39 +26,42 @@ namespace PswManagerTests.Commands {
         readonly EditCommand editCommand;
 
         public static IEnumerable<object[]> EditSuccessfullyData() {
+            static object[] NewObj(string name, string newName, string newPassword, string newEmail, string expected)
+                => new object[] {
+                    name,
+                    newName ?? name,
+                    ClassBuilder.Build(new EditCommand(null, null), new List<string> { newPassword, newName, newEmail, name }),
+                    expected
+                };
+
             var def = TestsHelper.DefaultValues;
 
-            //[0]args
-            //[1]newName
-            //[2]expected return string
-            yield return new object[] {
-                new[] { def.GetValue(1, DefaultValues.TypeValue.Name), "email:newEmail1", "password:newPassword1" },
-                def.GetValue(1, DefaultValues.TypeValue.Name),
-                $"{def.GetValue(1, DefaultValues.TypeValue.Name)} newPassword1 newEmail1"
-            };
-            yield return new object[] {
-                new[] { def.GetValue(2, DefaultValues.TypeValue.Name), "name:new:Name2", "email:newEmail2" },
-                "new:Name2",
+            yield return NewObj(def.GetValue(1, DefaultValues.TypeValue.Name), 
+                null, "newPassword1", "newEmail1", 
+                $"{def.GetValue(1, DefaultValues.TypeValue.Name)} newPassword1 newEmail1");
+
+            yield return NewObj(def.GetValue(2, DefaultValues.TypeValue.Name),
+                "new:Name2", null, "newEmail2",
                 $"new:Name2 {def.GetValue(2, DefaultValues.TypeValue.Password)} newEmail2"
-            };
-            yield return new object[] {
-                new[] { def.GetValue(3, DefaultValues.TypeValue.Name), "password:passfix", "name:fixedName", "email:fixed@email.com" },
-                "fixedName",
-                "fixedName passfix fixed@email.com"
-            };
+                );
+
+            yield return NewObj(def.GetValue(3, DefaultValues.TypeValue.Name),
+                "fixedName", "passfix", "fixed@email.com",
+                "fixedName passfix fixed@email.com");
+
         }
 
         [Theory]
         [MemberData(nameof(EditSuccessfullyData))]
-        public void EditSuccessfully(string[] args, string newName, string expected) {
+        public void EditSuccessfully(string name, string newName, ICommandInput args, string expected) {
 
             //arrange
             TestsHelper.SetUpDefault();
 
             //act
-            var previous = getCommand.Run(new string[] { args[0] });
+            var previous = getCommand.Run(new string[] { name });
             editCommand.Run(args);
-            var actual = getCommand.Run(new string[] { newName });
+            var actual = getCommand.Run(new string[] { newName ?? name });
 
             //assert
             Assert.NotEqual(previous, actual);
@@ -66,34 +70,39 @@ namespace PswManagerTests.Commands {
         }
 
         public static IEnumerable<object[]> ExpectedValidationFailuresData() {
-            string validName = TestsHelper.DefaultValues.GetValue(0, DefaultValues.TypeValue.Name);
+            static object[] NewObj(string errorMessage, string name, string newName, string newPassword, string newEmail)
+                => new object[] {
+                    errorMessage,
+                    ClassBuilder.Build(new EditCommand(null, null), new List<string> { newPassword, newName, newEmail, name })
+                };
+            //string validName = TestsHelper.DefaultValues.GetValue(0, DefaultValues.TypeValue.Name);
 
-            yield return new object[] { ValidationCollection.ArgumentsNullMessage, null };
+            //yield return new object[] { ValidationCollection.ArgumentsNullMessage, null };
 
-            yield return new object[] { ValidationCollection.ArgumentsNullOrEmptyMessage, new string[] { null } };
-            yield return new object[] { ValidationCollection.ArgumentsNullOrEmptyMessage, new string[] { "", "name:newvalue" } };
-            yield return new object[] { ValidationCollection.ArgumentsNullOrEmptyMessage, new string[] { "     ", "password:tiehwgfuh" } };
-            yield return new object[] { ValidationCollection.ArgumentsNullOrEmptyMessage, new string[] { validName, "password:tiehwgfuh", null } };
+            //yield return new object[] { ValidationCollection.ArgumentsNullOrEmptyMessage, new string[] { null } };
+            //yield return new object[] { ValidationCollection.ArgumentsNullOrEmptyMessage, new string[] { "", "name:newvalue" } };
+            //yield return new object[] { ValidationCollection.ArgumentsNullOrEmptyMessage, new string[] { "     ", "password:tiehwgfuh" } };
+            //yield return new object[] { ValidationCollection.ArgumentsNullOrEmptyMessage, new string[] { validName, "password:tiehwgfuh", null } };
 
-            yield return new object[] { new ValidationCollection(null).InexistentAccountMessage(), new string[] { "fakeAccountName", "password:newPasshere" } };
+            yield return NewObj(new ValidationCollection(null).InexistentAccountMessage(), "fakeAccountName", null, "newPasshere", null);
 
-            yield return new object[] { ValidationCollection.WrongArgumentsNumberMessage, Array.Empty<string>() };
-            yield return new object[] { ValidationCollection.WrongArgumentsNumberMessage, new string[] { validName } };
-            yield return new object[] { ValidationCollection.WrongArgumentsNumberMessage, new string[] { validName, "password:newPassword1", "email:email@somewhere.com", "name:newName", "password:newvalidPassword" } };
+            //yield return new object[] { ValidationCollection.WrongArgumentsNumberMessage, Array.Empty<string>() };
+            //yield return new object[] { ValidationCollection.WrongArgumentsNumberMessage, new string[] { validName } };
+            //yield return new object[] { ValidationCollection.WrongArgumentsNumberMessage, new string[] { validName, "password:newPassword1", "email:email@somewhere.com", "name:newName", "password:newvalidPassword" } };
 
-            yield return new object[] { EditCommand.InvalidSyntaxMessage, new string[] { validName, "newPassword" } };
-            yield return new object[] { EditCommand.InvalidSyntaxMessage, new string[] { validName, "pass@eqwwr" } };
+            //yield return new object[] { EditCommand.InvalidSyntaxMessage, new string[] { validName, "newPassword" } };
+            //yield return new object[] { EditCommand.InvalidSyntaxMessage, new string[] { validName, "pass@eqwwr" } };
 
-            yield return new object[] { EditCommand.InvalidKeyFound, new string[] { validName, "password:qrqrewqe", "nam:newname" } };
-            yield return new object[] { EditCommand.InvalidKeyFound, new string[] { validName, "name:newname", "password:qweqwed" , "ema:email@thisone.com"} };
+            //yield return new object[] { EditCommand.InvalidKeyFound, new string[] { validName, "password:qrqrewqe", "nam:newname" } };
+            //yield return new object[] { EditCommand.InvalidKeyFound, new string[] { validName, "name:newname", "password:qweqwed" , "ema:email@thisone.com"} };
 
-            yield return new object[] { EditCommand.DuplicateKeyFound, new string[] { validName, "email:email@somewhere.com", "email:someEma@here.com" } };
-            yield return new object[] { EditCommand.DuplicateKeyFound, new string[] { validName, "password:newPassword1", "email:email@somewhere.com", "password:newvalidPassword" } };
+            //yield return new object[] { EditCommand.DuplicateKeyFound, new string[] { validName, "email:email@somewhere.com", "email:someEma@here.com" } };
+            //yield return new object[] { EditCommand.DuplicateKeyFound, new string[] { validName, "password:newPassword1", "email:email@somewhere.com", "password:newvalidPassword" } };
         }
 
         [Theory]
         [MemberData(nameof(ExpectedValidationFailuresData))]
-        public void ExpectedValidationFailures(string expectedErrorMessage, params string[] args) {
+        public void ExpectedValidationFailures(string expectedErrorMessage, ICommandInput args) {
 
             //arrange
             bool valid;
