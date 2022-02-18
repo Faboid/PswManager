49a1371d77bc2d8890091347e125e2d7a17e5c0e
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-18 22:31:55 +0100

    Implemented extensions for generic-using IValidationCollection(s).

--- PswManagerLibrary/Extensions/IValidationCollectionExtensions.cs
+++ PswManagerLibrary/Extensions/IValidationCollectionExtensions.cs
@@ -8,6 +8,8 @@ namespace PswManagerLibrary.Extensions {
         //todo - turn this into a constant value. Consider moving it to ValidationCollection
         public static string InexistentAccountMessage(this IValidationCollection collection) => "The given account doesn't exist.";
 
+        public static string InexistentAccountMessage<T>(this IValidationCollection<T> collection) => "The given account doesn't exist.";
+
         /// <summary>
         /// Adds a condition to make sure the account exists.
         /// </summary>
@@ -15,6 +17,14 @@ namespace PswManagerLibrary.Extensions {
         public static void AddAccountShouldExistCondition(this IValidationCollection collection, ushort index, IDataHelper dataHelper) {
             collection.Add(new IndexHelper(index, collection.NullIndexCondition, collection.NullOrEmptyArgsIndexCondition, collection.CorrectArgsNumberIndexCondition), 
                 (args) => dataHelper.AccountExist(args[0]) == true, collection.InexistentAccountMessage());
+        }
+
+        /// <summary>
+        /// Adds a condition to make sure the account exists.
+        /// </summary>
+        /// <param name="pswManager"></param>
+        public static void AddAccountShouldExistCondition<T>(this IValidationCollection<T> collection, ushort index, string name, IDataHelper dataHelper) {
+            collection.Add(index, dataHelper.AccountExist(name) == true, collection.InexistentAccountMessage());
         }
 
     }
