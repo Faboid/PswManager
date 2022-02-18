19e56d3d55ca7e5f5f5c146e59f9b105e45bdffe
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-18 18:49:57 +0100

    Implemented a ClassBuilder class as a helper for testing commands. More on its purpose below:
    The reason of this implementation is to decouple the dependency on the current IInputObject used by each class.
    By creating the object dynamically, the tests won't break even when changing the input object(as long as the properties remain the same).

---
+++ PswManagerTests/Commands/Helper/ClassBuilder.cs
@@ -0,0 +1,30 @@
+﻿using PswManagerLibrary.UIConnection.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerTests.Commands.Helper {
+    internal class ClassBuilder {
+
+        public static object Build(Type type, List<string> args) {
+
+            object output = Activator.CreateInstance(type);
+
+            var props = type
+                .GetProperties()
+                .Where(x => x.GetCustomAttribute<RequestAttribute>() != null)
+                .OrderByDescending(x => x.Name);
+
+            var zip = props.Zip(args, (p, a) => new { Prop = p, Arg = a });
+            foreach(var item in zip) {
+                item.Prop.SetValue(output, item.Arg);
+            }
+
+            return output;
+        }
+
+    }
+}
