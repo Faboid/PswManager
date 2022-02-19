8019df459a219b4b0e196bec586bcf55a4b61117
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-19 19:00:39 +0100

    Migrated GetAllCommand to the new BaseCommand<> parent.

--- PswManagerLibrary/Commands/GetAllCommand.cs
+++ PswManagerLibrary/Commands/GetAllCommand.cs
@@ -1,21 +1,34 @@
 ﻿using PswManagerCommands;
 using PswManagerCommands.AbstractCommands;
+using PswManagerCommands.TempLocation;
 using PswManagerCommands.Validation;
 using PswManagerDatabase.DataAccess.Interfaces;
 using PswManagerDatabase.Models;
 using PswManagerLibrary.Cryptography;
+using PswManagerLibrary.UIConnection.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace PswManagerLibrary.Commands {
-    public class GetAllCommand : BaseCommand {
+    public class GetAllCommand : BaseCommand<GetAllCommand.KeyArgs> {
 
         readonly IDataReader dataReader;
         readonly ICryptoAccount cryptoAccount;
         readonly string[] validKeys = new string[] { "names", "passwords", "emails" };
         public const string InexistentKeyErrorMessage = "Invalid key(s) has been found. Valid keys: names, passwords, emails.";
         public const string DuplicateKeyErrorMessage = "Duplicate keys aren't allowed.";
+
+        public class KeyArgs : ICommandInput {
+
+            [Request("Keys", true, "Leave empty if you want all values.", 
+                "If you only want specific ones, insert their keys: names, passwords, emails.",
+                "Properly put a single space between the keys, if you require multiple.")]
+            public string Keys { get; private set; }
+
+            public IEnumerable<string> SplitKeys() => Keys.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x));
+
+        }
 
         public GetAllCommand(IDataReader dataReader, ICryptoAccount cryptoAccount) {
             this.dataReader = dataReader;
@@ -26,27 +39,24 @@ namespace PswManagerLibrary.Commands {
             return "Gets a list of the requested parameters of all accounts.";
         }
 
-        public override string GetSyntax() {
-            return $"get-all { string.Join(' ', validKeys.Select(x => $"[{x}]?")) }";
-        }
+        protected override IValidationCollection<KeyArgs> AddConditions(IValidationCollection<KeyArgs> collection) {
+            var obj = collection.GetObject();
 
-        protected override IValidationCollection AddConditions(IValidationCollection collection) {
-            collection.AddCommonConditions(0, validKeys.Length);
-            collection.Add(new IndexHelper(0, collection.NullOrEmptyArgsIndexCondition), (args) => args.All(x => validKeys.Contains(x)), InexistentKeyErrorMessage);
-            collection.Add(new IndexHelper(1, 0), (args) => args.Distinct().Count() == args.Length, DuplicateKeyErrorMessage);
+            collection.Add(0, obj.SplitKeys().All(x => validKeys.Contains(x)), InexistentKeyErrorMessage);
+            collection.Add(1, obj.SplitKeys().Distinct().Count() == obj.SplitKeys().Count(), DuplicateKeyErrorMessage);
 
             return collection;
         }
 
-        protected override CommandResult RunLogic(string[] arguments) {
+        protected override CommandResult RunLogic(KeyArgs arguments) {
             var result = dataReader.GetAllAccounts();
 
-            if(arguments.Length == 0) { 
+            if(string.IsNullOrWhiteSpace(arguments.Keys)) { 
                 return new CommandResult("The list has been retrieved.", true, string.Join(Environment.NewLine, result.Value.Select(Decrypt)));
             }
-            bool getNames = arguments.Contains(validKeys[0]);
-            bool getPasswords = arguments.Contains(validKeys[1]);
-            bool getEmails = arguments.Contains(validKeys[2]);
+            bool getNames = arguments.SplitKeys().Contains(validKeys[0]);
+            bool getPasswords = arguments.SplitKeys().Contains(validKeys[1]);
+            bool getEmails = arguments.SplitKeys().Contains(validKeys[2]);
 
             var accounts = result.Value
                 .Select(Decrypt)
