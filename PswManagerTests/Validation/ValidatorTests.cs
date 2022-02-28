720c7c493cc3d4b88589df25c76d2cc9d9fb43af
Faboid <72757782+Faboid@users.noreply.github.com>
2022-02-28 18:05:24 +0100

    Implemented test for Validator<T>'s use of AutoValidation<T>.

--- PswManagerTests/Validation/ValidatorTests.cs
+++ PswManagerTests/Validation/ValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using PswManagerCommands.Validation;
+using PswManagerCommands.Validation.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,8 +19,15 @@ namespace PswManagerTests.Validation {
             string minimumAgeMessage = "The minimum required age is 13.";
             Condition<TestObject> ageCondition = new(new IndexHelper(2), (obj) => obj.Age > 13, minimumAgeMessage);
 
+
+            AutoValidationBuilder<TestObject> autoValBuilder = new();
+            AutoValidation<TestObject> autoValidator = autoValBuilder
+                .AddRule(new ValidateNotEmpty())
+                .Build();
+
             ValidatorBuilder<TestObject> builder = new();
             Validator<TestObject> validator = builder
+                .AddAutoValidator(autoValidator)
                 .AddCondition(new IndexHelper(0), (obj) => !string.IsNullOrWhiteSpace(obj.Name), missingNameMessage)
                 .AddCondition(new IndexHelper(1, 0), (obj) => obj.Name.Length > 2, minimumNameLengthMessage)
                 .AddCondition(ageCondition)
@@ -27,16 +35,20 @@ namespace PswManagerTests.Validation {
 
             TestObject obj1 = new("rightuy", "Name here", 15);
             TestObject obj2 = new("rigrrree", "y", 3);
+            TestObject obj3 = new("", "validName", 77);
 
             //act
             var err_1 = validator.Validate(obj1);
             var err_2 = validator.Validate(obj2);
+            var err_3 = validator.Validate(obj3);
 
             //assert
             Assert.Empty(err_1);
             Assert.NotEmpty(err_2);
             Assert.Contains(minimumNameLengthMessage, err_2);
             Assert.Contains(minimumAgeMessage, err_2);
+            Assert.NotEmpty(err_3);
+            Assert.Contains("Temporary error message: value not valid", err_3);
 
         }
 
@@ -55,9 +67,22 @@ namespace PswManagerTests.Validation {
             Age = age;
         }
 
+        [NotEmpty]
         public string Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
 
     }
+
+    public class ValidateNotEmpty : ValidationRule {
+        public ValidateNotEmpty() : base(typeof(NotEmptyAttribute), typeof(string)) { }
+
+        protected override bool InnerLogic(Attribute attribute, object value) {
+            return !string.IsNullOrEmpty((string)value);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class NotEmptyAttribute : Attribute { }
+
 }
