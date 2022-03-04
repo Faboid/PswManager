1216f4f4da946104815f76c1ad5b47a2145d371f
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-04 15:17:54 +0100

    Extracted usage of GetProperty() and null handling to TryGetProperty() extension.

--- PswManagerTests/Commands/Helper/ErrorReader.cs
+++ PswManagerTests/Commands/Helper/ErrorReader.cs
@@ -4,15 +4,24 @@ using System;
 using System.Reflection;
 
 namespace PswManagerTests.Commands.Helper {
-    public static class ErrorReader {
+    internal static class ErrorReader {
 
         public static string GetRequiredError(this ICommand cmd, string propertyName) {
-            var prop = cmd.GetCommandInputType.GetProperty(propertyName);
-            return prop.GetCustomAttribute<RequiredAttribute>().GetErrorMessage(prop);
+            var prop = cmd
+                .GetCommandInputType
+                .TryGetProperty(propertyName);
+
+            return prop
+                .GetCustomAttribute<RequiredAttribute>()
+                .GetErrorMessage(prop);
         }
 
         public static string GetError<TAttribute>(this ICommand cmd, string propertyName) where TAttribute : RuleAttribute {
-            return cmd.GetCommandInputType.GetProperty(propertyName).GetCustomAttribute<TAttribute>().ErrorMessage;
+            return cmd
+                .GetCommandInputType
+                .TryGetProperty(propertyName)
+                .GetCustomAttribute<TAttribute>()
+                .ErrorMessage;
         }
 
         public static string GetRequiredError<TCommand>(string propertyName) where TCommand : ICommand {
@@ -37,20 +46,24 @@ namespace PswManagerTests.Commands.Helper {
         private static PropertyInfo GetProperty<TCommand>(this string propertyName) {
             try {
 
-                var prop = typeof(TCommand).BaseType //get parent object
+                return typeof(TCommand).BaseType //get parent object
                     .GetGenericArguments()[0] //get the generic given to the parent object; the ICommandInput type
-                    .GetProperty(propertyName); //get the specific property
-
-                if(prop == null) {
-                    throw ExceptionsFactory.CreateArgException(propertyName);
-                }
-
-                return prop;
+                    .TryGetProperty(propertyName); //get the specific property
 
             } catch(IndexOutOfRangeException) {
 
                 throw ExceptionsFactory.CreateInvCastException<TCommand>("the error reader", "ErrorReader.GetErrorMessage() or ErrorReader.GetRequiredErrorMessage()");
             }
+        }
+
+        private static PropertyInfo TryGetProperty(this Type type, string propertyName) {
+            var prop = type.GetProperty(propertyName);
+
+            if(prop == null) {
+                throw ExceptionsFactory.CreateArgException(propertyName);
+            }
+
+            return prop;
         }
 
     }
