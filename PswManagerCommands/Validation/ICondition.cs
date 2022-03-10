76c9de3caf3f4e80cf7e6016f2260825b6380519
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-10 18:15:33 +0100

    Extracted all the additional classes from ValidatorBuilder's file.

---
+++ PswManagerCommands/Validation/ICondition.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PswManagerCommands.Validation {
+    public interface ICondition<T> {
+        string GetErrorMessage();
+        bool IsValid(T obj);
+        bool IsValid(T obj, IList<int> failedConditions);
+    }
+
+}
--- PswManagerCommands/Validation/ValidatorBuilder.cs
+++ PswManagerCommands/Validation/ValidatorBuilder.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
 
-//todo - split this file into multiple with each one class/interface
 [assembly:InternalsVisibleTo("PswManagerTests")]
 namespace PswManagerCommands.Validation {
     public class ValidatorBuilder<T> {
@@ -30,94 +28,6 @@ namespace PswManagerCommands.Validation {
 
         public IValidator<T> Build() {
             return new Validator<T>(conditions, autoValidators);
-        }
-
-    }
-
-    public interface ICondition<T> {
-
-        string GetErrorMessage();
-        bool IsValid(T obj);
-        bool IsValid(T obj, IList<int> failedConditions);
-    }
-
-    public class Condition<T> : ICondition<T> {
-
-        public Condition(IndexHelper index, Func<T, bool> logic, string errorMessage) {
-            Index = index;
-            Logic = logic;
-            ErrorMessage = errorMessage;
-        }
-
-        private readonly string ErrorMessage;
-        public IndexHelper Index { get; init; }
-        public Func<T, bool> Logic { get; init; }
-
-        public string GetErrorMessage() => ErrorMessage;
-
-        public bool IsValid(T obj) => IsValid(obj, new List<int>());
-        public bool IsValid(T obj, IList<int> failedConditions) {
-
-            //if a required condition has failed, return true
-            //reason being that it's unecessary to pile the user with unnecessary error messages
-            if(Index.RequiredSuccesses.Any(x => failedConditions.Contains(x))) {
-                return true;
-            }
-
-            try {
-
-                //if the validation passes, return true
-                if(Logic.Invoke(obj)) {
-                    return true;
-                }
-            } catch {
-                return false;
-            }
-
-            failedConditions.Add(Index.Index);
-            return false;
-        }
-
-    }
-
-    public interface IValidator<T> {
-
-        IEnumerable<string> Validate(T obj);
-
-    }
-
-    internal class Validator<T> : IValidator<T> {
-
-        internal Validator(IReadOnlyCollection<ICondition<T>> conditions, List<IAutoValidator<T>> autoValidator) {
-            this.conditions = conditions;
-            this.autoValidator = autoValidator;
-        }
-
-        readonly IReadOnlyCollection<ICondition<T>> conditions;
-        readonly List<IAutoValidator<T>> autoValidator;
-
-        /// <summary>
-        /// Runs the given object through a list of prebuild conditions.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns>The errors in string format. If there's none, the validation was successful.</returns>
-        public IEnumerable<string> Validate(T obj) {
-            var errors = autoValidator.Select(x => x.Validate(obj)).SelectMany(x => x).ToList();
-            foreach(var err in errors) {
-                yield return err;
-            }
-
-            List<int> failedConditions = new();
-            if(errors.Any()) {
-                failedConditions.Add(-1);
-            }
-
-            foreach(var cond in conditions) {
-                if(!cond.IsValid(obj, failedConditions)) {
-                    yield return cond.GetErrorMessage();
-                }
-            }
-
         }
 
     }
