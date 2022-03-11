dce5adeb6a09839ce03040328c43fcbfe36ad367
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-11 21:59:29 +0100

    Added NuGet System.Data.SqlClient to database csproj.

---
+++ PswManagerDatabase/DataAccess/SQLDatabase/SQLConnection.cs
@@ -0,0 +1,39 @@
+﻿using PswManagerDatabase.Config;
+using PswManagerDatabase.DataAccess.SQLDatabase.SQLConnHelper;
+using PswManagerDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PswManagerDatabase.DataAccess.SQLDatabase {
+    internal class SQLConnection : IDataConnection {
+
+        public bool AccountExist(string name) {
+            throw new NotImplementedException();
+        }
+
+        public ConnectionResult CreateAccount(AccountModel model) {
+            throw new NotImplementedException();
+        }
+
+        public ConnectionResult DeleteAccount(string name) {
+            throw new NotImplementedException();
+        }
+
+        public ConnectionResult<AccountModel> GetAccount(string name) {
+            throw new NotImplementedException();
+        }
+
+        public ConnectionResult<List<AccountModel>> GetAllAccounts() {
+            throw new NotImplementedException();
+        }
+
+        public IPaths GetPaths() {
+            throw new NotImplementedException();
+        }
+
+        public ConnectionResult<AccountModel> UpdateAccount(string name, AccountModel newModel) {
+            throw new NotImplementedException();
+        }
+    }
+}
