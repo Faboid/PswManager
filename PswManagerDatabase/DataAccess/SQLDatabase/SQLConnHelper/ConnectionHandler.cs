a53abf22833a3f60a72d59d159c2af3976c192e4
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-11 22:03:12 +0100

    Implemented helper method to open & close a SqlConnection automatically through try-catch.

---
+++ PswManagerDatabase/DataAccess/SQLDatabase/SQLConnHelper/ConnectionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerDatabase.DataAccess.SQLDatabase.SQLConnHelper {
+    internal static class ConnectionHandler {
+
+        public static T Open<T>(this SqlConnection cnn, Func<T> predicate) {
+            try {
+                cnn.Open();
+
+                return predicate.Invoke();
+
+            } finally {
+                if(cnn.State == System.Data.ConnectionState.Open) {
+                    cnn.Close();
+                }
+            }
+        }
+
+    }
+}
