d53732031616079c573e8a1b17fa2a14842cef5e
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-11 22:04:08 +0100

    Implemented a SqlCommand builder(only the CreateAccount query so far).

---
+++ PswManagerDatabase/DataAccess/SQLDatabase/SQLConnHelper/QueriesBuilder.cs
@@ -0,0 +1,26 @@
+﻿using PswManagerDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerDatabase.DataAccess.SQLDatabase.SQLConnHelper {
+    internal class QueriesBuilder {
+
+        private const string accountsTable = "Accounts";
+
+        public static SqlCommand BuildCreateAccountQuery(AccountModel model) {
+            string query = $"insert into {accountsTable}" +
+                $"values(@Name, @Password, @Email)";
+            var cmd = new SqlCommand(query);
+            cmd.Parameters.Add(new SqlParameter("@Name", model.Name));
+            cmd.Parameters.Add(new SqlParameter("@Password", model.Password));
+            cmd.Parameters.Add(new SqlParameter("@Email", model.Email));
+
+            return cmd;
+        }
+
+    }
+}
