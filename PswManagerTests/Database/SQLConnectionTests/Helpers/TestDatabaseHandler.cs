5a1e478ae7ac0ca10ea19298d0b60d676a42bd85
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-13 18:32:06 +0100

    Added prefix to the test database name to not conflict with the production one.

---
+++ PswManagerTests/Database/SQLConnectionTests/Helpers/TestDatabaseHandler.cs
@@ -0,0 +1,43 @@
+﻿using PswManagerDatabase;
+using PswManagerDatabase.DataAccess.Interfaces;
+using PswManagerDatabase.Models;
+using PswManagerTests.TestsHelpers;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace PswManagerTests.Database.SQLConnectionTests.Helpers {
+    internal class TestDatabaseHandler : IDisposable {
+
+        public TestDatabaseHandler(string dbName) {
+            DatabaseName = $"TestDB_{dbName}";
+            dbPath = Path.Combine(WorkingDirectory, "Data", $"{DatabaseName}.db");
+            defaultValues = new DefaultValues(5);
+        }
+
+        private static readonly string WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private readonly string dbPath;
+        public IDataCreator dataCreator;
+        public DefaultValues defaultValues;
+        public string DatabaseName;
+
+        public void SetUpDefaultValues() {
+            //reset database
+            File.Delete(dbPath);
+            dataCreator = new DataFactory(DatabaseName).GetDataCreator();
+
+            for(int i = 0; i < defaultValues.values.Count; i++) {
+                var name = defaultValues.GetValue(i, DefaultValues.TypeValue.Name);
+                var password = defaultValues.GetValue(i, DefaultValues.TypeValue.Password);
+                var email = defaultValues.GetValue(i, DefaultValues.TypeValue.Email);
+
+                var model = new AccountModel(name, password, email);
+                dataCreator.CreateAccount(model);
+            }
+        }
+
+        public void Dispose() {
+            File.Delete(dbPath);
+        }
+    }
+}
