66839ab497b079815a8eb7317d92718ac7e39335
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-13 18:33:08 +0100

    Implemented tests to check whether IDataCreator properly creates accounts when using the SQLite database.

---
+++ PswManagerTests/Database/SQLConnectionTests/DataCreator.cs
@@ -0,0 +1,63 @@
+﻿using PswManagerDatabase;
+using PswManagerDatabase.DataAccess.Interfaces;
+using PswManagerDatabase.Models;
+using PswManagerTests.TestsHelpers;
+using Xunit;
+using System;
+using PswManagerTests.Database.SQLConnectionTests.Helpers;
+
+namespace PswManagerTests.Database.SQLConnectionTests {
+
+    public class DataCreator : IDisposable {
+
+        public DataCreator() {
+            dbHandler = new(db_Name);
+            IDataFactory dataFactory = new DataFactory(dbHandler.DatabaseName);
+            dataCreator = dataFactory.GetDataCreator();
+        }
+
+        const string db_Name = "DataCreatorTestsDB";
+        readonly IDataCreator dataCreator;
+        readonly TestDatabaseHandler dbHandler;
+
+        [Fact]
+        public void CreateAccountCorrectly() {
+
+            //arrange
+            dbHandler.SetUpDefaultValues();
+            AccountModel account = new("newLovelyAccount", "girhwugrrigjth", "eco@email.yo");
+
+            //act
+            bool exist = dataCreator.AccountExist(account.Name);
+            var result = dataCreator.CreateAccount(account);
+
+            //assert
+            Assert.False(exist);
+            Assert.True(result.Success);
+            Assert.True(dataCreator.AccountExist(account.Name));
+
+        }
+
+        [Fact]
+        public void CreateAccountFailure_AlreadyExists() {
+
+            //arrange
+            dbHandler.SetUpDefaultValues();
+            AccountModel account = new(dbHandler.defaultValues.GetValue(0, DefaultValues.TypeValue.Name), "password", "email");
+
+            //act
+            bool exist = dataCreator.AccountExist(account.Name);
+            var result = dataCreator.CreateAccount(account);
+
+            //assert
+            Assert.True(exist);
+            Assert.False(result.Success);
+
+        }
+
+        public void Dispose() {
+            dbHandler.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
