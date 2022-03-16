2d187efb6147053f932ed782d6452c30436eceff
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-16 20:34:38 +0100

    Implemented DataHelper tests for the SQLite database methods.

---
+++ PswManagerTests/Database/SQLConnectionTests/DataHelper.cs
@@ -0,0 +1,44 @@
+﻿using PswManagerDatabase;
+using PswManagerDatabase.DataAccess.Interfaces;
+using PswManagerTests.Database.SQLConnectionTests.Helpers;
+using PswManagerTests.TestsHelpers;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PswManagerTests.Database.SQLConnectionTests {
+    public class DataHelper {
+
+        public DataHelper() {
+            dbHandler = new TestDatabaseHandler(db_Name);
+            IDataFactory dataFactory = new DataFactory(dbHandler.DatabaseName);
+            dataHelper = dataFactory.GetDataHelper();
+            TestsHelper.SetUpDefault();
+        }
+
+        const string db_Name = "DataHelperTestsDB";
+        readonly IDataHelper dataHelper;
+        readonly TestDatabaseHandler dbHandler;
+
+        public static IEnumerable<object[]> AccountExistTestsData() {
+            static object[] NewObj(string accName, bool shouldExist) => new object[] { accName, shouldExist };
+
+            yield return NewObj(DefaultValues.StaticGetValue(0, DefaultValues.TypeValue.Name), true);
+            yield return NewObj("rtoehognrkljnwigurehvbonolbneughwonko", false);
+            yield return NewObj("", false);
+            yield return NewObj("   ", false);
+            yield return NewObj(null, false);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(AccountExistTestsData))]
+        public void AccountExist(string name, bool shouldExist) {
+
+            //assert
+            dbHandler.SetUpDefaultValues();
+            Assert.True(dataHelper.AccountExist(name) == shouldExist);
+
+        }
+
+    }
+}
