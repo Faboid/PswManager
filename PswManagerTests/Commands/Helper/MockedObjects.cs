9680726675b4e636f12117e7414c76bd80ad45df
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-19 20:03:38 +0100

    Implemented GetEmptyCryptoString() to mock ICryptoString and set up additional method mocks for ICryptoAccount's mock.

--- PswManagerTests/Commands/Helper/MockedObjects.cs
+++ PswManagerTests/Commands/Helper/MockedObjects.cs
@@ -5,12 +5,26 @@ using PswManagerLibrary.UIConnection;
 namespace PswManagerTests.Commands.Helper {
     internal static class MockedObjects {
 
+        public static ICryptoString GetEmptyCryptoString() {
+            var cryptoString = new Mock<ICryptoString>();
+            cryptoString.Setup(x => x.Encrypt(It.IsAny<string>())).Returns<string>(x => x);
+            cryptoString.Setup(x => x.Decrypt(It.IsAny<string>())).Returns<string>(x => x);
+
+            return cryptoString.Object;
+        }
+
         /// <summary>
         /// Gets an ICryptoAccount that doesn't encrypt—it will return the same values that are given to it.
         /// </summary>
         /// <returns></returns>
         public static ICryptoAccount GetEmptyCryptoAccount() {
+            var cryptoString = GetEmptyCryptoString();
+
             var cryptoAccount = new Mock<ICryptoAccount>();
+            cryptoAccount.Setup(x => x.GetPassCryptoString()).Returns(cryptoString);
+            cryptoAccount.Setup(x => x.GetEmaCryptoString()).Returns(cryptoString);
+            cryptoAccount.Setup(x => x.Encrypt(It.IsAny<(string, string)>())).Returns<(string, string)>(x => x);
+            cryptoAccount.Setup(x => x.Decrypt(It.IsAny<(string, string)>())).Returns<(string, string)>(x => x);
             cryptoAccount.Setup(x => x.Encrypt(It.IsAny<string>(), It.IsAny<string>())).Returns<string, string>((pass, ema) => (pass, ema));
             cryptoAccount.Setup(x => x.Decrypt(It.IsAny<string>(), It.IsAny<string>())).Returns<string, string>((pass, ema) => (pass, ema));
 
