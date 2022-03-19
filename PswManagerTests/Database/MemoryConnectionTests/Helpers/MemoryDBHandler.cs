3df806ba6b882ec65b2c75be310a3417d1132e02
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-19 16:33:19 +0100

    Implemented additional constructor to generate a custom amount of default values.

--- PswManagerTests/Database/MemoryConnectionTests/Helpers/MemoryDBHandler.cs
+++ PswManagerTests/Database/MemoryConnectionTests/Helpers/MemoryDBHandler.cs
@@ -8,23 +8,28 @@ using System.Linq;
 namespace PswManagerTests.Database.MemoryConnectionTests.Helpers {
     internal class MemoryDBHandler {
 
-        public MemoryDBHandler() {
+        //constructs the class with a default value of five
+        public MemoryDBHandler() : this(5) { }
+
+        public MemoryDBHandler(int numDefaultValues) {
+            NumValues = numDefaultValues;
             factory = new DataFactory(DatabaseType.InMemory);
             dbConnection = factory.GetDataConnection();
-            defaultValues = new(numValues);
+            defaultValues = new(NumValues);
         }
+
+        public DefaultValues defaultValues { get; init; }
+        public int NumValues { get; init; }
 
         readonly DataFactory factory;
         readonly IDataConnection dbConnection;
-        public DefaultValues defaultValues;
-        readonly int numValues = 5;
 
         public MemoryDBHandler SetUpDefaultValues() {
             //reset database
             var accounts = dbConnection.GetAllAccounts().Value;
             accounts.ForEach(x => dbConnection.DeleteAccount(x.Name));
 
-            Enumerable.Range(0, numValues).ForEach(x => {
+            Enumerable.Range(0, NumValues).ForEach(x => {
                 var name = defaultValues.GetValue(x, DefaultValues.TypeValue.Name);
                 var password = defaultValues.GetValue(x, DefaultValues.TypeValue.Password);
                 var email = defaultValues.GetValue(x, DefaultValues.TypeValue.Email);
