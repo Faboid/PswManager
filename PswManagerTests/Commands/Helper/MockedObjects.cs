38a320c02812ce531d9b2b71ea6b2f541e3cea4d
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-19 18:10:43 +0100

    Decoupled AddCommandTests from TestsHelper.

---
+++ PswManagerTests/Commands/Helper/MockedObjects.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using PswManagerLibrary.Cryptography;
+
+namespace PswManagerTests.Commands.Helper {
+    internal static class MockedObjects {
+
+        /// <summary>
+        /// Gets an ICryptoAccount that doesn't encrypt—it will return the same values that are given to it.
+        /// </summary>
+        /// <returns></returns>
+        public static ICryptoAccount GetEmptyCryptoAccount() {
+            var cryptoAccount = new Mock<ICryptoAccount>();
+            cryptoAccount.Setup(x => x.Encrypt(It.IsAny<string>(), It.IsAny<string>())).Returns<string, string>((pass, ema) => (pass, ema));
+            cryptoAccount.Setup(x => x.Decrypt(It.IsAny<string>(), It.IsAny<string>())).Returns<string, string>((pass, ema) => (pass, ema));
+
+            return cryptoAccount.Object;
+        }
+
+    }
+}
