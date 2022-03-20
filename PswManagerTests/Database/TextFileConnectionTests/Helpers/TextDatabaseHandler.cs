7e5305abfed2e85259f6700013110a01bf4129fd
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-20 16:32:35 +0100

    Implemented database handler for TextFileConnection tests.

---
+++ PswManagerTests/Database/TextFileConnectionTests/Helpers/TextDatabaseHandler.cs
@@ -0,0 +1,56 @@
+﻿using PswManagerDatabase;
+using PswManagerDatabase.DataAccess.Interfaces;
+using PswManagerTests.TestsHelpers;
+using System;
+using System.IO;
+
+namespace PswManagerTests.Database.TextFileConnectionTests.Helpers {
+    internal class TextDatabaseHandler : IDisposable {
+
+        public TextDatabaseHandler(string dbName) : this(dbName, 5) { }
+
+        public TextDatabaseHandler(string dbName, int numValues) {
+            DatabaseName = $"TextTestDB_{dbName}";
+            dbFolder = Path.Combine(WorkingDirectory, "Data", DatabaseName);
+            DefaultValues = new DefaultValues(numValues);
+            paths = new CustomPaths(dbFolder);
+            CreateFiles();
+            factory = new DataFactory(paths);
+            dataCreator = factory.GetDataCreator();
+        }
+
+        public DefaultValues DefaultValues { get; init; }
+        private static readonly string WorkingDirectory = PswManagerHelperMethods.PathsBuilder.GetWorkingDirectory;
+        private readonly string dbFolder;
+        private readonly IDataCreator dataCreator;
+        private readonly DataFactory factory;
+        public readonly string DatabaseName;
+        private readonly CustomPaths paths;
+
+        public TextDatabaseHandler SetUpDefaultValues() {
+            CreateFiles();
+
+            foreach(var value in DefaultValues.values) {
+                var account = DefaultValues.ToAccount(value);
+                dataCreator.CreateAccount(account);
+            }
+
+            return this;
+        }
+
+        public DataFactory GetDBFactory() {
+            return factory;
+        }
+
+        private void CreateFiles() {
+            Directory.CreateDirectory(paths.WorkingDirectory);
+            File.Create(paths.AccountsFilePath).Close();
+            File.Create(paths.PasswordsFilePath).Close();
+            File.Create(paths.EmailsFilePath).Close();
+        }
+
+        public void Dispose() {
+            Directory.Delete(paths.WorkingDirectory, true);
+        }
+    }
+}
