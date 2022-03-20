00bd8d53a0571048517c15d54c652f708514fd92
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-20 19:36:44 +0100

    Removed dependency to TestsHelper by using the new Text Tests DB.

--- PswManagerTests/Database/TextFileConnectionTests/DataReader.cs
+++ PswManagerTests/Database/TextFileConnectionTests/DataReader.cs
@@ -1,41 +1,39 @@
-﻿using PswManagerDatabase;
-using PswManagerDatabase.DataAccess;
-using PswManagerDatabase.DataAccess.Interfaces;
+﻿using PswManagerDatabase.DataAccess.Interfaces;
 using PswManagerDatabase.Models;
 using PswManagerHelperMethods;
-using PswManagerLibrary.Storage;
+using PswManagerTests.Database.TextFileConnectionTests.Helpers;
 using PswManagerTests.TestsHelpers;
 using System.Linq;
 using Xunit;
 
 namespace PswManagerTests.Database.TextFileConnectionTests {
 
-    [Collection("TestHelperCollection")]
     public class DataReader {
 
         public DataReader() {
-            IDataFactory dataFactory = new DataFactory(TestsHelper.Paths);
-            reader = dataFactory.GetDataReader();
-            TestsHelper.SetUpDefault();
+            dbHandler = new TextDatabaseHandler(dbName, numValues).SetUpDefaultValues();
+            reader = dbHandler.GetDBFactory().GetDataReader();
         }
 
         readonly IDataReader reader;
+        readonly TextDatabaseHandler dbHandler;
+        const string dbName = "DataReaderTestsDB";
+        readonly int numValues = 3;
 
         [Fact]
         public void GetOneShouldReturn() {
 
             //arrange
             AccountModel expected = new(
-                TestsHelper.DefaultValues.GetValue(1, DefaultValues.TypeValue.Name),
-                TestsHelper.DefaultValues.GetValue(1, DefaultValues.TypeValue.Password),
-                TestsHelper.DefaultValues.GetValue(1, DefaultValues.TypeValue.Email)
+                dbHandler.DefaultValues.GetValue(1, DefaultValues.TypeValue.Name),
+                dbHandler.DefaultValues.GetValue(1, DefaultValues.TypeValue.Password),
+                dbHandler.DefaultValues.GetValue(1, DefaultValues.TypeValue.Email)
                 );
 
-            string name = TestsHelper.DefaultValues.GetValue(1, DefaultValues.TypeValue.Name);
+            string name = dbHandler.DefaultValues.GetValue(1, DefaultValues.TypeValue.Name);
 
             //act
             var actual = reader.GetAccount(name);
-            DecryptAccountValues(actual.Value);
 
             //assert
             AccountEqual(expected, actual.Value);
@@ -46,30 +44,25 @@ namespace PswManagerTests.Database.TextFileConnectionTests {
         public void GetAllShouldGetAll() {
 
             //arrange
-            var expectedAccounts = TestsHelper.DefaultValues.GetAll();
+            var expectedAccounts = new DefaultValues(numValues).GetAll();
 
             //act
             var actual = reader.GetAllAccounts().Value;
-            actual.ForEach(DecryptAccountValues);
 
             //assert
             Assert.Equal(expectedAccounts.Count, actual.Count);
 
             Enumerable
-                .Range(0, TestsHelper.DefaultValues.values.Count - 1)
+                .Range(0, numValues)
                 .ForEach(i => {
                     AccountEqual(expectedAccounts[i], actual[i]);
                 });
         }
 
-        private void AccountEqual(AccountModel expected, AccountModel actual) {
+        private static void AccountEqual(AccountModel expected, AccountModel actual) {
             Assert.Equal(expected.Name, actual.Name);
             Assert.Equal(expected.Password, actual.Password);
             Assert.Equal(expected.Email, actual.Email);
-        }
-
-        private void DecryptAccountValues(AccountModel account) {
-            (account.Password, account.Email) = TestsHelper.CryptoAccount.Decrypt(account.Password, account.Email);
         }
 
     }
