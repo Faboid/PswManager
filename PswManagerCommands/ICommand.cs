de2a733276a3c8bc0f7bc9b1204b140bae6354be
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-25 13:55:26 +0100

    Deleted unused interface.

--- PswManagerCommands/ICommand.cs
+++ PswManagerCommands/ICommand.cs
@@ -2,35 +2,6 @@
 using System.Collections.Generic;
 
 namespace PswManagerCommands {
-    public interface ICommandOld {
-
-        public Type GetCommandInputType { get; }
-
-        /// <summary>
-        /// Runs the arguments through a validation check, and, if they pass it, runs the command.
-        /// </summary>
-        /// <param name="arguments"></param>
-        /// <returns></returns>
-        CommandResult Run(string[] arguments);
-
-        /// <summary>
-        /// Validates the given arguments. If there's any failure, it returns <see langword="false"/> and a list with all the failures that occurred.
-        /// </summary>
-        /// <param name="arguments"></param>
-        /// <returns></returns>
-        (bool success, IEnumerable<string> errorMessages) Validate(string[] arguments);
-
-        /// <summary>
-        /// Gets a string that shows the syntax used by the command.
-        /// </summary>
-        string GetSyntax();
-
-        /// <summary>
-        /// Gets a string that describe the command in an user-friendly way.
-        /// </summary>
-        string GetDescription();
-
-    }
 
     /// <summary>
     /// Represents a command.
