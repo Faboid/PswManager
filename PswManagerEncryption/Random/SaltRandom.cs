e3daa7dce930cc23ed4aa35fc6d93812c3401174
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-26 11:38:52 +0100

    Implemented classes to generate a random number. More on the reason for it below:
    System.Random(int seed) could change number generation methods between platforms. To assure stability, it was necessary to create a custom, fixed number generator.

---
+++ PswManagerEncryption/Random/SaltRandom.cs
@@ -0,0 +1,22 @@
+﻿namespace PswManagerEncryption.Random {
+    internal class SaltRandom {
+
+        readonly int seed;
+        Range current;
+
+        public SaltRandom() : this(Environment.TickCount) { }
+
+        public SaltRandom(int seed) : this(34, 64, seed) { }
+
+        public SaltRandom(int min, int max, int seed) {
+            this.seed = seed;
+            current = new Range(min, max, seed);
+        }
+
+        public int Next() {
+            current += seed;
+            return current;
+        }
+
+    }
+}
