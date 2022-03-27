271506f3db96b2c38392f8b6fec9b5c15f2393c3
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-27 20:16:44 +0200

    Created JsonDBHandler and DataCreator tests for the JSON database. Using the new generic tests, the DataCreator code was limited to the constructor.

---
+++ PswManagerTests/Database/JsonConnectionTests/DataCreator.cs
@@ -0,0 +1,10 @@
+﻿using PswManagerTests.Database.Generic;
+using PswManagerTests.Database.JsonConnectionTests.Helpers;
+
+namespace PswManagerTests.Database.JsonConnectionTests {
+    public class DataCreator : DataCreatorGeneric {
+
+        public DataCreator() : base(new JsonDBHandler("DataCreatorTests")) { }
+
+    }
+}
---
+++ PswManagerTests/Database/JsonConnectionTests/Helpers/JsonDBHandler.cs
@@ -0,0 +1,52 @@
+﻿using PswManagerDatabase;
+using PswManagerHelperMethods;
+using PswManagerTests.Database.Generic;
+using PswManagerTests.TestsHelpers;
+using System.IO;
+
+namespace PswManagerTests.Database.JsonConnectionTests.Helpers {
+    internal class JsonDBHandler : ITestDBHandler {
+
+        public JsonDBHandler(string dbName) : this(dbName, 5) { }
+
+        public JsonDBHandler(string dbName, int numValues) {
+            DatabaseName = $"JsonTestsDB_{dbName}";
+            dbPath = Path.Combine(WorkingDirectory, "Data", DatabaseName);
+            factory = new DataFactory(DatabaseType.Json, DatabaseName);
+            defaultValues = new DefaultValues(numValues);
+        }
+
+        readonly string WorkingDirectory = PathsBuilder.GetWorkingDirectory;
+        readonly string DatabaseName;
+        readonly string dbPath;
+        readonly IDataFactory factory;
+        readonly DefaultValues defaultValues;
+
+        public ITestDBHandler SetUpDefaultValues() {
+            if(Directory.Exists(dbPath)) {
+                Directory.GetFiles(dbPath)
+                    .ForEach(x => File.Delete(x));
+            }
+            
+            foreach(var value in defaultValues.values) {
+                var account = DefaultValues.ToAccount(value);
+                factory.GetDataCreator().CreateAccount(account);
+            }
+
+            return this;
+        }
+
+        public IDataFactory GetDBFactory() {
+            return factory;
+        }
+
+        public DefaultValues GetDefaultValues() {
+            return defaultValues;
+        }
+        
+        public void Dispose() {
+            Directory.Delete(dbPath, true);
+        }
+
+    }
+}
