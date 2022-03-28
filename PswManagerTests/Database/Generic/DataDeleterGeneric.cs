bc03f0da519ce76738d7cd770249c10ea4a9310d
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-28 12:26:27 +0200

    Implemented DataDeleter's generic tests and used them for the JsonDB tests.

---
+++ PswManagerTests/Database/Generic/DataDeleterGeneric.cs
@@ -0,0 +1,56 @@
+﻿using PswManagerDatabase.DataAccess.Interfaces;
+using System;
+using Xunit;
+
+namespace PswManagerTests.Database.Generic {
+    public abstract class DataDeleterGeneric : IDisposable {
+
+        public DataDeleterGeneric(ITestDBHandler dbHandler) {
+            this.dbHandler = dbHandler.SetUpDefaultValues();
+            dataDeleter = dbHandler.GetDBFactory().GetDataDeleter();
+        }
+
+        readonly IDataDeleter dataDeleter;
+        readonly ITestDBHandler dbHandler;
+
+        [Fact]
+        public void DeleteAccountCorrectly() {
+
+            //arrange
+            string name = dbHandler.GetDefaultValues().GetValue(1, TestsHelpers.DefaultValues.TypeValue.Name);
+            bool exists;
+
+            //act
+            exists = dataDeleter.AccountExist(name);
+            var result = dataDeleter.DeleteAccount(name);
+
+            //assert
+            Assert.True(exists);
+            Assert.True(result.Success);
+            Assert.False(dataDeleter.AccountExist(name));
+        
+        }
+
+        [Fact]
+        public void DeleteFailure_NonExistentName() {
+
+            //arrange
+            string name = "gerobhipubihtsiyhrti";
+            bool exists;
+
+            //act
+            exists = dataDeleter.AccountExist(name);
+            var result = dataDeleter.DeleteAccount(name);
+
+            //assert
+            Assert.False(exists);
+            Assert.False(result.Success);
+
+        }
+
+        public void Dispose() {
+            dbHandler.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ PswManagerTests/Database/JsonConnectionTests/DataDeleter.cs
@@ -0,0 +1,12 @@
+﻿using PswManagerTests.Database.Generic;
+using PswManagerTests.Database.JsonConnectionTests.Helpers;
+
+namespace PswManagerTests.Database.JsonConnectionTests {
+    public class DataDeleter : DataDeleterGeneric {
+
+        public DataDeleter() : base(new JsonDBHandler("DataDeleterTests")) {
+
+        }
+
+    }
+}
