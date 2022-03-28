d6ead41cc6d9d42633bd7cd99ece0c191ea695ad
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-28 17:06:53 +0200

    Implemented DataHelperGeneric and used it to test JsonConnection's DB functionality.

---
+++ PswManagerTests/Database/Generic/DataHelperGeneric.cs
@@ -0,0 +1,44 @@
+﻿using PswManagerDatabase.DataAccess.Interfaces;
+using PswManagerTests.TestsHelpers;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PswManagerTests.Database.Generic {
+    public abstract class DataHelperGeneric : IDisposable {
+
+        public DataHelperGeneric(ITestDBHandler dbHandler) {
+            this.dbHandler = dbHandler.SetUpDefaultValues();
+            dataHelper = dbHandler.GetDBFactory().GetDataHelper();
+        }
+
+        readonly IDataHelper dataHelper;
+        readonly ITestDBHandler dbHandler;
+        protected static readonly int numValues = 1;
+
+        public static IEnumerable<object[]> AccountExistTestsData() {
+            static object[] NewObj(string name, bool shouldExist) => new object[] { name, shouldExist };
+
+            yield return NewObj(DefaultValues.StaticGetValue(0, DefaultValues.TypeValue.Name), true);
+            yield return NewObj("rtoehognrkljnwigurehvbonolbneughwonko", false);
+            yield return NewObj("", false);
+            yield return NewObj("   ", false);
+            yield return NewObj(null, false);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(AccountExistTestsData))]
+        public void AccountExist(string name, bool shouldExist) {
+
+            Assert.True(dataHelper.AccountExist(name) == shouldExist);
+
+        }
+
+        public void Dispose() {
+            dbHandler.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ PswManagerTests/Database/JsonConnectionTests/DataHelper.cs
@@ -0,0 +1,12 @@
+﻿using PswManagerTests.Database.Generic;
+using PswManagerTests.Database.JsonConnectionTests.Helpers;
+
+namespace PswManagerTests.Database.JsonConnectionTests {
+    public class DataHelper : DataHelperGeneric {
+
+        public DataHelper() : base(new JsonDBHandler("DataHelperTests", numValues)) {
+
+        }
+
+    }
+}
