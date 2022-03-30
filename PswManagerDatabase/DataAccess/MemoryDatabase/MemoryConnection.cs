6fcab41aa4b59e279860046e0b1d1f1d0c6dc835
Faboid <72757782+Faboid@users.noreply.github.com>
2022-03-30 22:40:20 +0200

    Implemented usage of new abstract base class to extract validation logic.

--- PswManagerDatabase/DataAccess/MemoryDatabase/MemoryConnection.cs
+++ PswManagerDatabase/DataAccess/MemoryDatabase/MemoryConnection.cs
@@ -3,60 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace PswManagerDatabase.DataAccess.MemoryDatabase {
-    internal class MemoryConnection : IDataConnection {
+    internal class MemoryConnection : BaseConnection {
 
         readonly Dictionary<string, AccountModel> accounts = new();
 
-        public bool AccountExist(string name) {
-            if(string.IsNullOrWhiteSpace(name)) {
-                return false;
-            }
-
+        protected override bool AccountExistHook(string name) {
             return accounts.ContainsKey(name);
         }
 
-        public ConnectionResult CreateAccount(AccountModel model) {
-            if(string.IsNullOrWhiteSpace(model.Name)) {
-                return new ConnectionResult(false, "The given name isn't valid.");
-            }
-            if(AccountExist(model.Name)) {
-                return new ConnectionResult(false, "The given account name is already occupied.");
-            }
-
+        protected override ConnectionResult CreateAccountHook(AccountModel model) {
             accounts.Add(model.Name, model);
             return new ConnectionResult(true);
         }
 
-        public ConnectionResult DeleteAccount(string name) {
-            if(!AccountExist(name)) {
-                return new ConnectionResult(false, "The given account doesn't exist.");
-            }
-
+        protected override ConnectionResult DeleteAccountHook(string name) {
             accounts.Remove(name);
             return new ConnectionResult(true);
         }
 
-        public ConnectionResult<AccountModel> GetAccount(string name) {
-            if(!AccountExist(name)) {
-                return new ConnectionResult<AccountModel>(false, "The given account doesn't exist.");
-            }
-
+        protected override ConnectionResult<AccountModel> GetAccountHook(string name) {
             return new ConnectionResult<AccountModel>(true, accounts[name]);
         }
 
-        public ConnectionResult<IEnumerable<AccountModel>> GetAllAccounts() {
+        protected override ConnectionResult<IEnumerable<AccountModel>> GetAllAccountsHook() {
             var list = accounts.Values.ToList();
             return new(true, list);
         }
 
-        public ConnectionResult<AccountModel> UpdateAccount(string name, AccountModel newModel) {
-            if(!AccountExist(name)) {
-                return new(false, "The given account doesn't exist.");
-            }
-            if(name != newModel.Name && AccountExist(newModel.Name)) {
-                return new(false, "There is already an account with that name.");
-            }
-
+        protected override ConnectionResult<AccountModel> UpdateAccountHook(string name, AccountModel newModel) {
             var account = accounts[name];
 
             if(!string.IsNullOrWhiteSpace(newModel.Password)) {
