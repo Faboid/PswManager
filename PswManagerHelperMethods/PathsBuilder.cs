8fb4f9a0260cb39fcd0db942eafc3f257ea20a1d
Faboid <72757782+Faboid@users.noreply.github.com>
2022-04-05 11:21:22 +0200

    Implemented new static path to gather all data in the same folder.

--- PswManagerHelperMethods/PathsBuilder.cs
+++ PswManagerHelperMethods/PathsBuilder.cs
@@ -4,7 +4,17 @@ using System.IO;
 namespace PswManagerHelperMethods {
     public static class PathsBuilder {
 
+        static PathsBuilder() {
+
+            //as some classes rely on this directory's existence,
+            //it's best to create it from here instead of doing a "worst case protection" by creating it in all of them
+            if(!Directory.Exists(GetDataDirectory)) {
+                Directory.CreateDirectory(GetDataDirectory);
+            }
+        }
+
         public static readonly string GetWorkingDirectory = Path.GetDirectoryName(Environment.ProcessPath);
+        public static readonly string GetDataDirectory = Path.Combine(GetWorkingDirectory, "Data");
 
     }
 }
