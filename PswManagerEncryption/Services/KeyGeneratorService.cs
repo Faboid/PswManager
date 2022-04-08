e8861ea65a4d604cf44815a6a229b037ab05d766
Faboid <72757782+Faboid@users.noreply.github.com>
2022-04-08 11:57:14 +0200

    Extracted Channel<T> to Async-focused assembly.

--- PswManagerEncryption/Services/KeyGeneratorService.cs
+++ PswManagerEncryption/Services/KeyGeneratorService.cs
@@ -1,6 +1,6 @@
 ﻿using PswManagerEncryption.Cryptography;
 using PswManagerEncryption.Random;
-using System.Collections.Concurrent;
+using PswManagerAsync;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
@@ -106,63 +106,6 @@ namespace PswManagerEncryption.Services {
             await generationTask; //this is to find any exception thrown in there
             rfc.Dispose();
             GC.SuppressFinalize(this);
-        }
-    }
-
-    internal class Channel<T> : IDisposable {
-
-        public Channel(int length) {
-            buffer = new();
-            cts = new();
-            Token = cts.Token;
-            readSemaphore = new(0);
-            writeSemaphore = new(length);
-        }
-
-        private readonly ConcurrentQueue<T> buffer;
-        private readonly SemaphoreSlim readSemaphore;
-        private readonly SemaphoreSlim writeSemaphore;
-        private readonly CancellationTokenSource cts;
-
-        public CancellationToken Token { get; }
-
-        //public async Task<bool> TryReadAsync(int milliseconds, out T? value) {
-        //    value = default;
-        //    bool entered = await readSemaphore.WaitAsync(milliseconds, Token);
-        //    if(!entered) {
-        //        return false;
-        //    }
-        //    bool success = buffer.TryDequeue(out value);
-        //    return success;
-        //}
-
-        public async Task<T> ReadAsync() {
-
-            T? output;
-            bool success;
-            do {
-                //if readSemaphore enters but there's no value, it was erranously released
-                //therefore, it's fine to lock it in "excess"
-                await readSemaphore.WaitAsync(Token);
-                success = buffer.TryDequeue(out output);
-            } while(!success);
-
-            writeSemaphore.Release();
-            return output!;
-        }
-
-        public async Task WriteAsync(T value) {
-            await writeSemaphore.WaitAsync(Token);
-            buffer.Enqueue(value);
-            readSemaphore.Release();
-        }
-
-        public void Dispose() {
-            cts.Cancel();
-            readSemaphore.Dispose();
-            writeSemaphore.Dispose();
-            cts.Dispose();
-            buffer.Clear();
         }
     }
 }
