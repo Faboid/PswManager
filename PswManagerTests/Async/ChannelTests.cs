264a3fdf9b47aff9e40b910ab2b65536cf5463b6
Faboid <72757782+Faboid@users.noreply.github.com>
2022-04-09 17:57:50 +0200

    Implemented test to check Channel's async blocking capabilities.

--- PswManagerTests/Async/ChannelTests.cs
+++ PswManagerTests/Async/ChannelTests.cs
@@ -1,9 +1,6 @@
 ﻿using PswManagerAsync;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -44,10 +41,61 @@ namespace PswManagerTests.Async {
         [Fact]
         public async void CorrectOrder() {
 
-            //todo
+            //arrange
+            Channel<int> channel = new(2);
+            int[] expected = { 1, 2, 3, 4 };
+            int[] actual = new int[4];
+
+            //act
+            Task<double> writeTask = Task.Run(async () => {
+                Stopwatch sw = Stopwatch.StartNew();
+                await channel.WriteAsync(expected[0]);
+                await channel.WriteAsync(expected[1]);
+                await channel.WriteAsync(expected[2]);
+                await channel.WriteAsync(expected[3]);
+                //extra values
+                await channel.WriteAsync(10);
+                await channel.WriteAsync(99);
+                sw.Stop();
+                return sw.Elapsed.TotalMilliseconds;
+            });
+            Task<double> readTask = Task.Run(async () => {
+                Stopwatch sw = Stopwatch.StartNew();
+                actual[0] = await TryWaitRead(channel);
+                actual[1] = await channel.ReadAsync();
+                actual[2] = await channel.ReadAsync();
+                await Task.Delay(200);
+                actual[3] = await channel.ReadAsync();
+                sw.Stop();
+                return sw.Elapsed.TotalMilliseconds;
+            });
+
+            //act
+            var timeoutTask = Task.Delay(1000);
+
+            //assert
+            if(await Task.WhenAny(writeTask, readTask, timeoutTask) == timeoutTask) {
+
+                throw new TimeoutException("The task is probably in a deadlock. The test has failed.");
+            }
+
+            await Task.WhenAll(writeTask, readTask);
+            //based on pre-established locks, "readTask" should finish before "writeTask",
+            //as WriteAsync() should be locked until the stored values are read
+            Assert.True(writeTask.Result > readTask.Result);
+            Assert.Equal(expected, actual);
 
         }
 
 
+        private static async Task<T> TryWaitRead<T>(Channel<T> channel) {
+            while(true) {
+                var (success, value) = await channel.TryReadAsync(500);
+
+                if(success) {
+                    return value;
+                }
+            }
+        }
     }
 }
