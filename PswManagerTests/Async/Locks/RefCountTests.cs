f46f30f6826f345e73eebe43418172c04e3c95d9
Faboid <72757782+Faboid@users.noreply.github.com>
2022-04-13 18:14:28 +0200

    Implemented test for RefCount<T>

---
+++ PswManagerTests/Async/Locks/RefCountTests.cs
@@ -0,0 +1,29 @@
+﻿using PswManagerAsync.Locks;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PswManagerTests.Async.Locks {
+    public class RefCountTests {
+
+        [Fact]
+        public async Task IsInUse_Correct() {
+
+            //arrange
+            RefCount<int> refCount = new(5);
+
+            //act
+            bool correctIsInUse = refCount.UseValue(x => refCount.IsInUse);
+            var task = refCount.UseValueAsync(async () => await Task.Delay(50));
+            bool asyncIsInUse = refCount.IsInUse;
+            await task;
+            bool afterAsyncIsInUse = refCount.IsInUse;
+            
+            //assert
+            Assert.True(correctIsInUse);
+            Assert.True(asyncIsInUse);
+            Assert.False(afterAsyncIsInUse);
+
+        }
+
+    }
+}
