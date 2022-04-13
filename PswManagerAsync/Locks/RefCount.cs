d365173a810adb3a981ee9ccc55c37b06fb07355
Faboid <72757782+Faboid@users.noreply.github.com>
2022-04-13 14:50:15 +0200

    Implemented RefCount; a class to keep track of whether a given object is being currently used.

---
+++ PswManagerAsync/Locks/RefCount.cs
@@ -0,0 +1,55 @@
+﻿namespace PswManagerAsync.Locks {
+    internal class RefCount<TValue> {
+
+        public RefCount(TValue value) {
+            this.value = value;
+            references = 0;
+        }
+
+        public bool IsInUse => references != 0;
+
+        private readonly TValue value;
+        private int references;
+
+        public async Task UseValueAsync(Func<Task> predicate) {
+            try {
+                references++;
+                await predicate.Invoke();
+            }
+            finally {
+                references--;
+            }
+        }
+
+        public async Task<T> UseValueAsync<T>(Func<TValue, Task<T>> predicate) {
+            try {
+                references++;
+                return await predicate.Invoke(value);
+            }
+            finally {
+                references--;
+            }
+        }
+
+        public void UseValue(Action<TValue> action) {
+            try {
+                references++;
+                action.Invoke(value);
+            }
+            finally {
+                references--;
+            }
+        }
+
+        public T UseValue<T>(Func<TValue, T> predicate) {
+            try {
+                references++;
+                return predicate.Invoke(value);
+            }
+            finally {
+                references--;
+            }
+        }
+
+    }
+}
