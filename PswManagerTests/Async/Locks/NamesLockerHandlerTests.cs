b6683f399e38cd2f375c5db6b06ad507b66345ec
Faboid <72757782+Faboid@users.noreply.github.com>
2022-04-18 09:38:36 +0200

    Implemented new test to check whether locks with different names are being differentiated.

--- PswManagerTests/Async/Locks/NamesLockerHandlerTests.cs
+++ PswManagerTests/Async/Locks/NamesLockerHandlerTests.cs
@@ -11,36 +11,75 @@ namespace PswManagerTests.Async.Locks {
 
             //arrange
             using NamesLockerHandler locker = new();
-            OrderChecker orderChecker = new();
+            using OrderChecker orderChecker = new();
             string lockName = "Hello!";
 
             //act
             var first = locker.LockHereAsync(lockName, async () => {
-                await Task.Delay(100);
-                orderChecker.Done(1);
+                await orderChecker.WaitFor(1, 200);
+                orderChecker.Done(2);
             }, 1000);
 
             //tests timeout mechanic
             LockResult secondResult = locker.LockHere(lockName, () => {
                 OrderChecker.Never();
-            }, 20);
+            }, 10);
 
             Task<LockResult> third = locker.LockHereAsync(lockName, () => {
-                orderChecker.Done(2);
+                orderChecker.Done(3);
             }, 1000);
 
+            orderChecker.Done(1);
             LockResult thirdResult = await third;
             LockResult firstResult = await first;
 
             //tests x.LockHere with no returns
-            LockResult fourthResult = locker.LockHere(lockName, () => orderChecker.Done(3), 50);
-            orderChecker.Done(4);
+            LockResult fourthResult = locker.LockHere(lockName, () => orderChecker.Done(4), 50);
+            orderChecker.Done(5);
 
             //assert
             Assert.True(firstResult.Success);
             Assert.True(secondResult.Failed);
             Assert.True(thirdResult.Success);
             Assert.True(fourthResult.Success);
+
+        }
+
+        [Fact]
+        public async Task DifferentiatesBetweenNames() {
+
+            //arrange
+            using NamesLockerHandler locker = new();
+            using OrderChecker orderChecker = new();
+            string lock1 = "One";
+            string lock2 = "Two";
+
+            //act
+            var firstLock = locker
+                .LockHereAsync(lock1, async () => {
+                        await orderChecker.WaitFor(1, 500);
+                        orderChecker.Done(2);
+                        await orderChecker.WaitFor(3, 500);
+                    }, 100)
+                .ThrowIfTakesOver(1000);
+
+            var firstShouldBeLocked = locker
+                .LockHereAsync(lock1, () => OrderChecker.Never(), 10)
+                .ThrowIfTakesOver(1000);
+
+            var secondResult = locker
+                .LockHere(lock2, () => orderChecker.Done(1), 50);
+
+            await orderChecker.WaitFor(2, 500);
+            orderChecker.Done(3);
+            var firstResult = await firstLock;
+            var firstShouldBeLockedResult = await firstShouldBeLocked;
+            orderChecker.Done(4);
+
+            //assert
+            Assert.True(firstResult.Success);
+            Assert.True(firstShouldBeLockedResult.Failed);
+            Assert.True(secondResult.Success);
 
         }
 
