b2c92fe92e577cb86bfb530c2556768e46b9986f
Faboid <72757782+Faboid@users.noreply.github.com>
2022-04-26 14:38:19 +0200

    Implemented tests to test LockAll() and LockAllAsync()'s functionality.

--- PswManagerTests/Async/Locks/NamesLockerTests.cs
+++ PswManagerTests/Async/Locks/NamesLockerTests.cs
@@ -27,7 +27,7 @@ namespace PswManagerTests.Async.Locks {
 
             async Task PartTwoLockLogic() {
                 orderChecker.Done(2);
-                await locker.GetLockAsync(lock2);
+                using var locktwo = await locker.GetLockAsync(lock2);
                 orderChecker.Done(4);
             }
 
@@ -50,6 +50,64 @@ namespace PswManagerTests.Async.Locks {
             Assert.True(heldLock1.Obtained);
             Assert.True(lockTaskResult.Item1.Obtained);
             Assert.False(lockTaskResult.Item2.Obtained);
+
+        }
+
+        [Fact]
+        public async Task SuccessfullyLockAll() {
+
+            //arrange
+            NamesLocker locker = new();
+            string name = "name";
+
+            //act
+            var namedLock = await locker.GetLockAsync(name, 10);
+            var nestedMainLock = await locker.GetAllLocksAsync(10);
+            nestedMainLock.Dispose();
+            namedLock.Dispose();
+
+            var mainLock = locker.GetAllLocks(50);
+            var nestedNamedLock = await locker.GetLockAsync(name, 10);
+            nestedNamedLock.Dispose();
+            mainLock.Dispose();
+
+            using var freedLock = await locker.GetLockAsync(name, 10);
+
+            //assert
+            Assert.True(namedLock.Obtained);
+            Assert.False(nestedMainLock.Obtained);
+            Assert.True(mainLock.Obtained);
+            Assert.False(nestedNamedLock.Obtained);
+            Assert.True(freedLock.Obtained);
+
+        }
+
+        [Fact]
+        public async Task LockAllFreesCorrectly() {
+
+            //arrange
+            NamesLocker locker = new();
+            string name = "name";
+
+            //act
+            var namedLock = locker.GetLock(name, 10);
+            var allLocksTask = locker.GetAllLocksAsync(100);
+            namedLock.Dispose();
+            var allLocks = await allLocksTask;
+
+            var failLock = await locker.GetLockAsync("someNewLock", 5);
+            failLock.Dispose();
+
+            var newNamedLockTask = locker.GetLockAsync(name, 100);
+            allLocks.Dispose();
+            var newNamedLock = await newNamedLockTask;
+            newNamedLock.Dispose();
+
+            //assert
+            Assert.True(namedLock.Obtained);
+            Assert.True(allLocks.Obtained);
+            Assert.False(failLock.Obtained);
+            Assert.True(newNamedLock.Obtained);
 
         }
 
