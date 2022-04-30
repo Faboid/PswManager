d5b6c63c02ba93d52504230fc908639c6096613b
Faboid <72757782+Faboid@users.noreply.github.com>
2022-04-30 19:30:45 +0200

    Rewrote TextFileConnection to change the database layout. Instead of storing everything in three files, every account is now stored in its own file.

---
+++ PswManagerDatabase/DataAccess/TextDatabase/TextFileConnHelper/FileSaver.cs
@@ -0,0 +1,122 @@
+﻿using PswManagerAsync.Locks;
+using PswManagerDatabase.Models;
+using PswManagerHelperMethods;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PswManagerDatabase.DataAccess.TextDatabase.TextFileConnHelper {
+    internal class FileSaver {
+
+        public FileSaver() : this("TextSaves") { }
+
+        public FileSaver(string customFolderName) {
+            if(string.IsNullOrWhiteSpace(customFolderName)) {
+                throw new ArgumentException("Given custom folder name is null or white spaces.", nameof(customFolderName));
+            }
+
+            directoryPath = Path.Combine(PathsBuilder.GetWorkingDirectory, "Data", customFolderName);
+            if(!Directory.Exists(directoryPath)) { 
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+
+        readonly string directoryPath;
+        private string BuildFilePath(string name) => Path.Combine(directoryPath, $"{name}.txt");
+
+        public bool Exists(string name) {
+            return File.Exists(BuildFilePath(name));
+        }
+
+        public void Create(AccountModel account) {
+            var path = BuildFilePath(account.Name);
+            var serialized = AccountSerializer.Serialize(account);
+            File.WriteAllLines(path, serialized);
+        }
+
+        public async Task CreateAsync(AccountModel account) {
+            var path = BuildFilePath(account.Name);
+            var serialized = AccountSerializer.Serialize(account);
+            await File.WriteAllLinesAsync(path, serialized);
+        }
+
+        public void Delete(string name) {
+            var path = BuildFilePath(name);
+            File.Delete(path);
+        }
+
+        public AccountModel Get(string name) {
+            var path = BuildFilePath(name);
+            var serialized = File.ReadAllLines(path);
+            return AccountSerializer.Deserialize(serialized);
+        }
+
+        public IEnumerable<ConnectionResult<AccountModel>> GetAll(NamesLocker locker) {
+            return Directory.GetFiles(directoryPath)
+                .AsParallel()
+                .Select(x => {
+                    var name = Path.GetFileNameWithoutExtension(x);
+                    using var nameLock = locker.GetLock(name, 5000);
+                    if(!nameLock.Obtained) {
+                        return new(false, $"The account {name} is being used elsewhere.");
+                    }
+                    if(!Exists(name)) {
+                        return new(false, $"The account {name} has been deleted or edited.");
+                    }
+
+                    var values = File.ReadAllLines(x);
+                    var account = AccountSerializer.Deserialize(values);
+                    return new ConnectionResult<AccountModel>(true, account);
+                });
+        }
+
+        public async Task<ConnectionResult<AccountModel>[]> GetAllAsync(NamesLocker locker) {
+            var tasks = Directory.GetFiles(directoryPath)
+                .AsParallel()
+                .Select(async x => {
+                    var name = Path.GetFileNameWithoutExtension(x);
+                    using var nameLock = await locker.GetLockAsync(name, 5000);
+                    if(!nameLock.Obtained) {
+                        return new(false, $"The account {name} is being used elsewhere.");
+                    }
+                    if(!Exists(name)) {
+                        return new(false, $"The account {name} has been deleted or edited.");
+                    }
+
+                    var values = await File.ReadAllLinesAsync(x);
+                    var account = AccountSerializer.Deserialize(values);
+                    return new ConnectionResult<AccountModel>(true, account);
+                });
+
+            return await Task.WhenAll(tasks);
+        }
+
+        public AccountModel Update(string name, AccountModel newModel) {
+            var path = BuildFilePath(name);
+            var values = File.ReadAllLines(path);
+
+            bool deleteOldFile = !string.IsNullOrWhiteSpace(newModel.Name) && name != newModel.Name;
+
+            if(!string.IsNullOrWhiteSpace(newModel.Name)) {
+                values[0] = newModel.Name;
+            }
+            if(!string.IsNullOrWhiteSpace(newModel.Password)) {
+                values[1] = newModel.Password;
+            }
+            if(!string.IsNullOrWhiteSpace(newModel.Email)) {
+                values[2] = newModel.Email;
+            }
+
+            var newPath = BuildFilePath(values[0]);
+            File.WriteAllLines(newPath, values);
+
+            if(deleteOldFile) {
+                File.Delete(path);
+            }
+
+            return Get(values[0]);
+        }
+    }
+}
--- PswManagerDatabase/DataAccess/TextDatabase/TextFileConnection.cs
+++ PswManagerDatabase/DataAccess/TextDatabase/TextFileConnection.cs
@@ -1,108 +1,167 @@
-﻿using PswManagerDatabase.DataAccess.TextDatabase.TextFileConnHelper;
+﻿using PswManagerAsync.Locks;
+using PswManagerDatabase.DataAccess.TextDatabase.TextFileConnHelper;
 using PswManagerDatabase.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PswManagerDatabase.DataAccess.TextDatabase {
-
     public class TextFileConnection : IDataConnection {
 
-        private readonly IPaths paths;
-        private readonly LengthCounter lengthCounter;
-        private readonly AccountBuilder accountBuilder;
-        private readonly AccountSearcher accountSearcher;
+        readonly static ConnectionResult invalidNameResult = new(false, "The given name is not valid.");
+        readonly static ConnectionResult usedElsewhereResult = new(false, "This account is being used elsewhere.");
+        readonly static ConnectionResult accountExistsAlreadyResult = new(false, "The given account exists already.");
+        readonly static ConnectionResult accountDoesNotExistResult = new(false, "The given account does not exist.");
+
+        internal TextFileConnection() {
+            fileSaver = new();
+        }
+
+        internal TextFileConnection(string customDB) {
+            fileSaver = new(customDB);
+        }
+
+        readonly NamesLocker locker = new();
+        readonly FileSaver fileSaver;
 
         /// <summary>
-        /// Instantiates <see cref="TextFileConnection"/> with the default path provided by <see cref="Paths"/>.
+        /// Checks if the account exists.
         /// </summary>
-        internal TextFileConnection() : this(new Paths()) { }
+        /// <param name="name"></param>
+        /// <returns></returns>
+        /// <exception cref="TimeoutException"></exception>
+        public bool AccountExist(string name) {
+            if(string.IsNullOrWhiteSpace(name)) {
+                return false;
+            }
 
-        /// <summary>
-        /// Instantiates <see cref="TextFileConnection"/> with custom paths.
-        /// </summary>
-        /// <param name="paths">The custom paths where to save the database.</param>
-        internal TextFileConnection(IPaths paths) {
-            this.paths = paths;
-            DatabaseHandler.SetUpMissingFiles(this.paths);
-            lengthCounter = new LengthCounter(this.paths);
-            accountBuilder = new AccountBuilder(this.paths);
-            accountSearcher = new AccountSearcher(this.paths);
+            using var accLock = locker.GetLock(name, 5000);
+            if(!accLock.Obtained) {
+                throw new TimeoutException($"The account {name} is being used elsewhere.");
+            }
+
+            return fileSaver.Exists(name);
         }
 
         public ConnectionResult CreateAccount(AccountModel model) {
             if(string.IsNullOrWhiteSpace(model.Name)) {
-                return new ConnectionResult(false, "The given name isn't valid.");
-            }
-            if(accountSearcher.AccountExist(model.Name)) {
-                return new ConnectionResult(false, "The given account name is already occupied.");
+                return invalidNameResult;
             }
 
-            accountBuilder.Create(model);
+            using var accLock = locker.GetLock(model.Name, 50);
+            if(!accLock.Obtained) {
+                return usedElsewhereResult;
+            }
 
-            lengthCounter.AddOne();
-            return new ConnectionResult(true);
+            if(fileSaver.Exists(model.Name)) {
+                return accountExistsAlreadyResult;
+            }
+
+            fileSaver.Create(model);
+            return new(true);
         }
 
         public async Task<ConnectionResult> CreateAccountAsync(AccountModel model) {
             if(string.IsNullOrWhiteSpace(model.Name)) {
-                return new ConnectionResult(false, "The given name isn't valid.");
-            }
-            if(accountSearcher.AccountExist(model.Name)) {
-                return new ConnectionResult(false, "The given account name is already occupied.");
+                return invalidNameResult;
             }
 
-            await accountBuilder.CreateAsync(model);
-            lengthCounter.AddOne();
-            return new ConnectionResult(true);
+            using var accLock = await locker.GetLockAsync(model.Name, 50);
+            if(!accLock.Obtained) {
+                return usedElsewhereResult;
+            }
+
+            if(fileSaver.Exists(model.Name)) {
+                return accountExistsAlreadyResult;
+            }
+
+            await fileSaver.CreateAsync(model);
+            return new(true);
+        }
+
+        public ConnectionResult DeleteAccount(string name) {
+            if(string.IsNullOrWhiteSpace(name)) {
+                return invalidNameResult;
+            }
+
+            using var accLock = locker.GetLock(name, 50);
+            if(!accLock.Obtained) {
+                return usedElsewhereResult;
+            }
+
+            if(!fileSaver.Exists(name)) {
+                return accountDoesNotExistResult;
+            }
+
+            fileSaver.Delete(name);
+            return new(true);
         }
 
         public ConnectionResult<AccountModel> GetAccount(string name) {
-            if(!accountSearcher.AccountExist(name, out int position)) {
-                return new ConnectionResult<AccountModel>(false, "The given account doesn't exist.");
+            if(string.IsNullOrWhiteSpace(name)) {
+                return invalidNameResult.ToConnectionResult<AccountModel>();
             }
 
-            AccountModel output = accountBuilder.Get(position);
+            using var accLock = locker.GetLock(name, 50);
+            if(!accLock.Obtained) {
+                return usedElsewhereResult.ToConnectionResult<AccountModel>();
+            }
 
-            return new ConnectionResult<AccountModel>(true, output);
+            if(!fileSaver.Exists(name)) {
+                return accountDoesNotExistResult.ToConnectionResult<AccountModel>();
+            }
+
+            var account = fileSaver.Get(name);
+            return new(true, account);
         }
 
+        //todo - turn this into ConnectionResult<IEnumerable<ConnectionResult<AccountModel>>>
         public ConnectionResult<IEnumerable<AccountModel>> GetAllAccounts() {
 
-            IEnumerable<AccountModel> accounts = accountBuilder.GetAll();
+            //as returning empty AccountModel(s) will throw when decrypting,
+            //the timeout values are temporarily filtered off.
+            var accounts = fileSaver.GetAll(locker)
+                .Where(x => x.Success)
+                .Select(x => x.Value);
 
             return new(true, accounts);
         }
 
         public ConnectionResult<AccountModel> UpdateAccount(string name, AccountModel newModel) {
-
-            if(!accountSearcher.AccountExist(name, out int position)) {
-                return new ConnectionResult<AccountModel>(false, "The given account doesn't exist.");
-            }
-            if(name != newModel.Name && AccountExist(newModel.Name)) {
-                return new(false, "There is already an account with that name.");
+            if(string.IsNullOrWhiteSpace(name)) {
+                return invalidNameResult.ToConnectionResult<AccountModel>();
             }
 
-            lengthCounter.ValidateLength(position);
+            using var nameLock = locker.GetLock(name, 50);
+            if(!nameLock.Obtained) {
+                return usedElsewhereResult.ToConnectionResult<AccountModel>();
+            }
 
-            accountBuilder.Edit(position, newModel);
+            if(!fileSaver.Exists(name)) {
+                return accountDoesNotExistResult.ToConnectionResult<AccountModel>();
+            }
 
-            return GetAccount(string.IsNullOrWhiteSpace(newModel.Name)? name : newModel.Name);
+            NamesLocker.Lock newModelLock = null;
+            try {
+                if(!string.IsNullOrWhiteSpace(newModel.Name) && name != newModel.Name) {
+                    newModelLock = locker.GetLock(newModel.Name, 50);
+                    if(!newModelLock.Obtained) {
+                        return usedElsewhereResult.ToConnectionResult<AccountModel>();
+                    }
+                    if(fileSaver.Exists(newModel.Name)) {
+                        return new(false, $"There is already an account called {newModel.Name}.");
+                    }
+                }
+
+                var newValues = fileSaver.Update(name, newModel);
+                return new(true, newValues);
+
+            } finally {
+                newModelLock?.Dispose();
+            }
+
         }
-
-        public ConnectionResult DeleteAccount(string name) {
-            if(!accountSearcher.AccountExist(name, out int position)) {
-                return new ConnectionResult(false, "The given account doesn't exist.");
-            }
-
-            lengthCounter.ValidateLength(position);
-
-            accountBuilder.Delete(position);
-
-            lengthCounter.SubtractOne();
-            return new ConnectionResult(true);
-        }
-
-        public bool AccountExist(string name) => accountSearcher.AccountExist(name);
-
     }
 }
