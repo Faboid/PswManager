9729f9436e196c771c0f2d3c0d02e00bf602cc3c
Faboid <72757782+Faboid@users.noreply.github.com>
2022-05-02 20:20:04 +0200

    Implemented extension methods for IAsyncEnumerable.

---
+++ PswManagerHelperMethods/IAsyncEnumerableExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PswManagerHelperMethods {
+    public static class IAsyncEnumerableExtensions {
+
+        public static async Task<List<T>> Take<T>(this IAsyncEnumerable<T> enumerable, int count) {
+            List<T> list = new();
+
+            int curr = 0;
+            await foreach(var item in enumerable) {
+                if(curr >= count) {
+                    break;
+                }
+                list.Add(item);
+                curr++;
+            }
+
+            return list;
+        }
+
+        public static async Task<List<T>> ToList<T>(this IAsyncEnumerable<T> enumerable) {
+            List<T> list = new();
+            await foreach(var item in enumerable) {
+                list.Add(item);
+            }
+            return list;
+        }
+
+        public static async IAsyncEnumerable<U> Select<T, U>(this IAsyncEnumerable<T> enumerable, Func<T, U> selector) { 
+            await foreach(var item in enumerable) {
+                yield return selector(item);
+            }
+        }
+
+        public static async IAsyncEnumerable<U> Select<T, U>(this IAsyncEnumerable<T> enumerable, Func<T, Task<U>> selector) {
+            await foreach(var item in enumerable) {
+                yield return await selector(item);
+            }
+        }
+
+    }
+}
