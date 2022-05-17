fa3c917c98e61290649058dbe035ba5b9b71d937
Faboid <72757782+Faboid@users.noreply.github.com>
2022-05-17 18:34:27 +0200

    (untested)Implemented Result, Result<T>, and AsyncResult, AsyncResult<T>.
    The async versions inherit directly from Task<Result> and Task<Result<T>> respectively.

---
+++ PswManager.Utils/WrappingObjects/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace PswManager.Utils.WrappingObjects; 
+
+public class Result {
+
+    public Result(bool success) {
+        Success = success;
+    }
+
+    public Result(string errorMessage) {
+        Success = false;
+        ErrorMessage = errorMessage;
+    }
+
+    public bool Success { get; init; }
+    public bool Failed => !Success;
+    public string ErrorMessage { get; init; } = string.Empty;
+    public bool HasErrorMessage => !string.IsNullOrWhiteSpace(ErrorMessage);
+
+}
+
+public class Result<TValue> : Result {
+
+    public Result(bool success) : base(success) { }
+
+    public Result(string errorMessage) : base(errorMessage) { }
+    public Result(TValue value) : base(true) {
+        Value = value;
+    }
+
+    public TValue Value { get; init; }
+
+}
