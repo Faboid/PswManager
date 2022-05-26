604665fa7fb0b59af822dfd646c9537eada03612
Faboid <72757782+Faboid@users.noreply.github.com>
2022-05-26 20:05:21 +0200

    Implemented tests for AccountDeleter's happy path.

---
+++ PswManager.Core.Tests/Inner/AccountDeleterTests.cs
@@ -0,0 +1,60 @@
+﻿using Moq;
+using PswManager.Core.Inner;
+using PswManager.Database.DataAccess.Interfaces;
+using PswManager.Database.Models;
+using Xunit;
+
+namespace PswManager.Core.Tests.Inner {
+    public class AccountDeleterTests {
+
+        public AccountDeleterTests() {
+
+            dataDeleter = new();
+
+            dataDeleter
+                .Setup(x => x.DeleteAccount(It.IsAny<string>()))
+                .Returns<string>(x => new ConnectionResult(!string.IsNullOrWhiteSpace(x)));
+
+            dataDeleter
+                .Setup(x => x.DeleteAccountAsync(It.IsAny<string>()))
+                .Returns<string>(x => ValueTask.FromResult(new ConnectionResult(!string.IsNullOrWhiteSpace(x))));
+        }
+
+        readonly Mock<IDataDeleter> dataDeleter;
+
+        [Theory]
+        [InlineData("someName")]
+        public void DeleteAccountCallsDBCorrectly(string name) {
+
+            //arrange
+            AccountDeleter deleter = new(dataDeleter.Object);
+
+            //act
+            var result = deleter.DeleteAccount(name);
+
+            //assert
+            Assert.True(result.Success);
+            dataDeleter.Verify(x => x.DeleteAccount(It.Is<string>(x => x == name)));
+            dataDeleter.VerifyNoOtherCalls();
+
+        }
+
+        [Theory]
+        [InlineData("someName")]
+        public async Task DeleteAccountCallsDBCorrectlyAsync(string name) {
+
+            //arrange
+            AccountDeleter deleter = new(dataDeleter.Object);
+
+            //act
+            var result = await deleter.DeleteAccountAsync(name);
+
+            //assert
+            Assert.True(result.Success);
+            dataDeleter.Verify(x => x.DeleteAccountAsync(It.Is<string>(x => x == name)));
+            dataDeleter.VerifyNoOtherCalls();
+
+        }
+
+    }
+}
