19bc04ab07530e0e9c2b2890fd802f66ff7ba82a
Faboid <72757782+Faboid@users.noreply.github.com>
2022-05-26 20:09:43 +0200

    Added TODO.cs file to remind of future general addition.

---
+++ PswManager.Core.Tests/Inner/TODO.cs
@@ -0,0 +1,10 @@
+﻿/*
+
+Testing the bad paths of the AccountManager wrapping classes.
+
+Why it's being delayed?
+The CommandResult<T> class might replace its string-based error message in favor of a enum-based status result.
+To properly test the bad paths, I plan to make sure the correct error codes are thrown out.
+As it's currently still using the string-based error, there's no point adding those now.
+
+*/
