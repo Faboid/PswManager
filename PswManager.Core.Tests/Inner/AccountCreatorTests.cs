cf6281f8e80406a60235c610f0a34fa25ab45dd5
Faboid <72757782+Faboid@users.noreply.github.com>
2022-05-26 18:38:43 +0200

    Implemented tests for AccountCreator. More blow:
    The already-implemented CryptoService class generates a salt for each encryption, so I've mocked ICryptoService to produce consistent result.
    Might extract the mocking logic later on.

---
+++ PswManager.Core.Tests/Inner/AccountCreatorTests.cs
@@ -0,0 +1,98 @@
+﻿using Moq;
+using PswManager.Core.Cryptography;
+using PswManager.Core.Inner;
+using PswManager.Database.DataAccess.Interfaces;
+using PswManager.Database.Models;
+using PswManager.Encryption.Services;
+using Xunit;
+
+namespace PswManager.Core.Tests.Inner {
+    public class AccountCreatorTests {
+
+        public AccountCreatorTests() {
+            var mockPassCryptoService = new Mock<ICryptoService>();
+            mockPassCryptoService
+                .Setup(x => x.Encrypt(It.IsAny<string>()))
+                .Returns<string>(x => new string(x?.Reverse().ToArray() ?? throw new NullReferenceException()));
+
+            var mockEmaCryptoService = new Mock<ICryptoService>();
+            mockEmaCryptoService
+                .Setup(x => x.Encrypt(It.IsAny<string>()))
+                .Returns<string>(x => x?.Aggregate((x, y) => (char)(x + y)).ToString() ?? throw new NullReferenceException());
+
+            cryptoAccount = new CryptoAccount(mockPassCryptoService.Object, mockEmaCryptoService.Object);
+
+            dataCreatorMock = new Mock<IDataCreator>();
+            dataCreatorMock
+                .Setup(x => x.CreateAccount(It.IsAny<AccountModel>()))
+                .Returns<AccountModel>(MockConnectionResult);
+            dataCreatorMock
+                .Setup(x => x.CreateAccountAsync(It.IsAny<AccountModel>()))
+                .Returns<AccountModel>(x => Task.FromResult(MockConnectionResult(x)));
+        }
+
+        //since the purpose of this class is encrypting the password & email,
+        //and then passing the values down to the database, there's no need to test the database itself.
+        readonly Mock<IDataCreator> dataCreatorMock;
+
+        //a single version to produce consistent results
+        readonly ICryptoAccount cryptoAccount;
+
+        [Theory]
+        [InlineData("nameHere", "passPass", "emaema")]
+        public void AccountObjectGotEncrypted(string name, string password, string email) {
+
+            //arrange
+            var (creator, input, expected) = ArrangeTest(name, password, email);
+
+            //act
+            var result = creator.CreateAccount(input);
+
+            //assert
+            Assert.True(result.Success);
+            dataCreatorMock.Verify(x => x.CreateAccount(It.Is<AccountModel>(x => AssertEqual(expected, x))));
+
+        }
+
+        [Theory]
+        [InlineData("asyncName", "newpass", "ema@here.com")]
+        public async Task AccountObjectGotEncryptedAsync(string name, string password, string email) {
+
+            //arrange
+            var (creator, input, expected) = ArrangeTest(name, password, email);
+
+            //act
+            var result = await creator.CreateAccountAsync(input);
+
+            //assert
+            Assert.True(result.Success);
+            dataCreatorMock.Verify(x => x.CreateAccountAsync(It.Is<AccountModel>(x => AssertEqual(expected, x))));
+
+        }
+
+        private static ConnectionResult MockConnectionResult(AccountModel model) {
+            bool[] isAnyNullOrEmpty = new bool[] {
+                string.IsNullOrWhiteSpace(model.Name),
+                string.IsNullOrWhiteSpace(model.Password),
+                string.IsNullOrWhiteSpace(model.Email)
+            };
+
+            return new ConnectionResult(!isAnyNullOrEmpty.Any(x => x));
+        }
+
+        private (AccountCreator creator, AccountModel input, AccountModel expected) ArrangeTest(string name, string password, string email) {
+            var creator = new AccountCreator(dataCreatorMock.Object, cryptoAccount);
+            var input = new AccountModel(name, password, email);
+            var expected = cryptoAccount.Encrypt(input);
+            return (creator, input, expected);
+        }
+
+        private static bool AssertEqual(AccountModel expected, AccountModel actual) {
+            Assert.Equal(expected.Name, actual.Name);
+            Assert.Equal(expected.Password, actual.Password);
+            Assert.Equal(expected.Email, actual.Email);
+            return true;
+        }
+
+    }
+}
