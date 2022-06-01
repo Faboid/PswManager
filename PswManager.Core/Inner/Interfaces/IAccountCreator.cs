bbdf17202b05f3d47a5dcc23ab06a8014f741011
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-01 18:08:36 +0200

    Set interface visibility to public.

--- PswManager.Core/Inner/Interfaces/IAccountCreator.cs
+++ PswManager.Core/Inner/Interfaces/IAccountCreator.cs
@@ -3,7 +3,7 @@ using PswManager.Utils.WrappingObjects;
 using System.Threading.Tasks;
 
 namespace PswManager.Core.Inner.Interfaces {
-    interface IAccountCreator {
+    public interface IAccountCreator {
 
         Result CreateAccount(AccountModel model);
         Task<Result> CreateAccountAsync(AccountModel model);
