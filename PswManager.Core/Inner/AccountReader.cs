cce0bcc3933c40f9a597d20b6c91ed4d0e7bb132
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-02 15:52:29 +0200

    Hotfix - Added decryption of "ReadAll" elements. It was accidentally removed previously.

--- PswManager.Core/Inner/AccountReader.cs
+++ PswManager.Core/Inner/AccountReader.cs
@@ -2,9 +2,11 @@
 using PswManager.Core.Inner.Interfaces;
 using PswManager.Database.DataAccess.Interfaces;
 using PswManager.Database.Models;
+using PswManager.Utils;
 using PswManager.Utils.WrappingObjects;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PswManager.Core.Inner {
@@ -41,7 +43,7 @@ namespace PswManager.Core.Inner {
         public Result<IEnumerable<AccountResult>> ReadAllAccounts() {
             var result = dataReader.GetAllAccounts();
             return result.Success switch {
-                true => new(result.Value),
+                true => new(result.Value.Select(x => Decrypt(x))),
                 false => new(result.ErrorMessage)
             };
         }
@@ -49,7 +51,7 @@ namespace PswManager.Core.Inner {
         public async Task<Result<IAsyncEnumerable<AccountResult>>> ReadAllAccountsAsync() {
             var result = await dataReader.GetAllAccountsAsync();
             return result.Success switch {
-                true => new(result.Value),
+                true => new(result.Value.Select(DecryptAsync)),
                 false => new(result.ErrorMessage)
             };
         }
@@ -72,6 +74,26 @@ namespace PswManager.Core.Inner {
         /// <param name="result"></param>
         /// <returns></returns>
         private Task<Result<AccountModel>> DecryptAsync(ConnectionResult<AccountModel> result)
+            => Task.Run(() => Decrypt(result));
+
+        /// <summary>
+        /// If <paramref name="result"/>.Success is:<br/>
+        /// <br/><see langword="true"/>: Decrypts the account model and returns a successful result.
+        /// <br/><see langword="false"/> Returns a failure result with <paramref name="result"/>.ErrorMessage.
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private AccountResult Decrypt(AccountResult result) => result.Success switch {
+            true => new(result.NameAccount, cryptoAccount.Decrypt(result.Value)),
+            false => result
+        };
+
+        /// <summary>
+        /// <see cref="Task.Run{AccountModel}(Func{AccountModel})"/> wrapper of <see cref="Decrypt(AccountResult)"/>.
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private Task<AccountResult> DecryptAsync(AccountResult result)
             => Task.Run(() => Decrypt(result));
 
     }
