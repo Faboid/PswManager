df79aa5bd00ff1af806c50f0d68503ae5928d105
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-03 21:46:13 +0200

    (untested) Implemented Option<TValue, TError> to better express the result of methods.

---
+++ PswManager.Utils/Option.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace PswManager.Utils;
+
+public interface IOption<TValue, TError> {
+
+    T Match<T>(Func<TValue, T> some, Func<TError, T> error, Func<T> none);
+    IOption<T, TError> Next<T>(Func<TValue, IOption<T, TError>> func);
+
+}
+
+public struct Option<TValue, TError> : IOption<TValue, TError> {
+
+    private readonly IOption<TValue, TError> option = new None<TValue, TError>();
+
+    public Option(TValue value) {
+        option = new Some<TValue, TError>(value);
+    }
+
+    public Option(TError error) {
+        option = new Error<TValue, TError>(error);
+    }
+
+    public Option() {
+        option = new None<TValue, TError>();
+    }
+
+    public T Match<T>(Func<TValue, T> some, Func<TError, T> error, Func<T> none) => option.Match(some, error, none);
+    public IOption<T, TError> Next<T>(Func<TValue, IOption<T, TError>> func) => option.Next(func);
+
+}
+
+internal class Some<TValue, TError> : IOption<TValue, TError> {
+
+    private readonly TValue value;
+
+    internal Some(TValue value) {
+        this.value = value;
+    }
+
+    public T Match<T>(Func<TValue, T> some, Func<TError, T> error, Func<T> none) => some.Invoke(value);
+    public IOption<T, TError> Next<T>(Func<TValue, IOption<T, TError>> func) => func.Invoke(value);
+
+}
+
+internal class Error<TValue, TError> : IOption<TValue, TError> {
+
+    private readonly TError err;
+
+    public Error(TError error) {
+        err = error;
+    }
+
+    public T Match<T>(Func<TValue, T> some, Func<TError, T> error, Func<T> none) => error.Invoke(err);
+    public IOption<T, TError> Next<T>(Func<TValue, IOption<T, TError>> func) => new Error<T, TError>(err);
+
+}
+
+internal class None<TValue, TError> : IOption<TValue, TError> {
+
+    public None() { }
+
+    public T Match<T>(Func<TValue, T> some, Func<TError, T> error, Func<T> none) => none.Invoke();
+    public IOption<T, TError> Next<T>(Func<TValue, IOption<T, TError>> func) => new None<T, TError>();
+}
