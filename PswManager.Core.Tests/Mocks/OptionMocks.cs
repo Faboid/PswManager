6ad8daa26a10f26bff97359c69131e9e1aaf788e
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-07 22:07:08 +0200

    Implemented class to mock an option result.

---
+++ PswManager.Core.Tests/Mocks/OptionMocks.cs
@@ -0,0 +1,18 @@
+﻿using PswManager.Database.DataAccess.ErrorCodes;
+using PswManager.Database.Models;
+using PswManager.Utils;
+
+namespace PswManager.Core.Tests.Mocks {
+    internal class OptionMocks {
+
+        public static Option<CreatorErrorCode> ValidateValues(AccountModel model) {
+
+            if(string.IsNullOrWhiteSpace(model.Name)) return CreatorErrorCode.InvalidName;
+            if(string.IsNullOrWhiteSpace(model.Password)) return CreatorErrorCode.MissingPassword;
+            if(string.IsNullOrWhiteSpace(model.Email)) return CreatorErrorCode.MissingEmail;
+
+            return Option.None<CreatorErrorCode>();
+        }
+
+    }
+}
