32b4ad74c1caea95867146649b11b9052ba1a192
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-07 16:47:03 +0200

    Implemented a Unit type, a representation of void that can be used with generics.

---
+++ PswManager.Utils/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+namespace PswManager.Utils; 
+
+/// <summary>
+/// Represents void.
+/// </summary>
+[StructLayout(LayoutKind.Sequential, Size = 1)]
+public sealed class Unit {
+
+    /// <summary>
+    /// Gets an empty instance of <see cref="Unit"/>.
+    /// </summary>
+    public readonly static Unit Get = new();
+    private Unit() { }
+
+}
