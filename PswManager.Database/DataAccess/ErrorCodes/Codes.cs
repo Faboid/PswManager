1109cb1b069bb36ea2c76ad8660ba630db13a9a7
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-09 22:48:38 +0200

    Implemented error codes for editing values.

--- PswManager.Database/DataAccess/ErrorCodes/Codes.cs
+++ PswManager.Database/DataAccess/ErrorCodes/Codes.cs
@@ -34,5 +34,28 @@ public enum DeleterErrorCode {
 }
 
 public enum EditorErrorCode {
+    /// <summary>
+    /// The error is undefined. This could either created with <see langword="default"/> or because an unknown error happened.
+    /// </summary>
     Undefined,
+    /// <summary>
+    /// The given name is null, empty, or whitespace.
+    /// </summary>
+    InvalidName,
+    /// <summary>
+    /// The account is being used elsewhere.
+    /// </summary>
+    UsedElsewhere,
+    /// <summary>
+    /// The given name does not exist.
+    /// </summary>
+    DoesNotExist,
+    /// <summary>
+    /// The new name is being used elsewhere. Might be because this edit request has been launched multiple times, or because that new name is occupied.
+    /// </summary>
+    NewNameUsedElsewhere,
+    /// <summary>
+    /// The new name exists already, so the account cannot be use it.
+    /// </summary>
+    NewNameExistsAlready,
 }
