361aa39b82a31f18e2f872d4695f2a3ce4a4ed9e
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-09 17:05:04 +0200

    Implemented AsValueTask, an extension method to convert any T object into a ValueTask<T> object.

--- PswManager.Extensions/TaskExtensions.cs
+++ PswManager.Extensions/TaskExtensions.cs
@@ -3,5 +3,6 @@
 public static class TaskExtensions {
 
     public static Task<T> AsTask<T>(this T value) => Task.FromResult(value);
+    public static ValueTask<T> AsValueTask<T>(this T value) => ValueTask.FromResult(value);
 
 }
