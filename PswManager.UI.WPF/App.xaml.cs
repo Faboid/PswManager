7ece7312a1c72a49885fa58961ae34d385be8a36
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-23 12:11:06 +0200

    Added basic framework for the WPF UI.

---
+++ PswManager.UI.WPF/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.Windows;
+
+namespace PswManager.UI.WPF;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+
+    private readonly IHost _host;
+
+	public App() {
+		_host = Host.CreateDefaultBuilder()
+			.UseSerilog((host, loggerConfiguration) => {
+				loggerConfiguration
+					.MinimumLevel.Debug()
+					.WriteTo.Debug()
+					.WriteTo.File("Log.txt", rollingInterval: RollingInterval.Hour); //todo - put a centralized path
+
+			})
+			.ConfigureServices(services => {
+
+			}).Build();
+	}
+
+	protected override void OnStartup(StartupEventArgs e) {
+		_host.Start();
+		base.OnStartup(e);
+	}
+}
