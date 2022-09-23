cd75f63c983c6c3a0a036f267c4cbea4ce70397b
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-23 14:13:53 +0200

    Added commands to guarantee the basic window functioning.

---
+++ PswManager.UI.WPF/Commands/CloseCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace PswManager.UI.WPF.Commands;
+
+public class CloseCommand : CommandBase {
+
+    private readonly Window _window;
+
+    public CloseCommand(Window window) {
+        _window = window;
+    }
+
+    public override void Execute(object? parameter) {
+        _window.Close();
+    }
+
+}
---
+++ PswManager.UI.WPF/Commands/RelayCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PswManager.UI.WPF.Commands;
+
+public class RelayCommand : CommandBase {
+
+    private readonly Action _action;
+
+    public RelayCommand(Action action) {
+        _action = action;
+    }
+
+    public override void Execute(object? parameter) => _action.Invoke();
+}
