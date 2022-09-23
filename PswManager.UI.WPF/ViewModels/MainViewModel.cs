35f456ed0b13824112af123302ea52fe0ba453a3
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-23 14:16:43 +0200

    Implemented MainWindow and its viewmodel.

---
+++ PswManager.UI.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,59 @@
+using PswManager.UI.WPF.Commands;
+using PswManager.UI.WPF.Services;
+using PswManager.UI.WPF.Stores;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PswManager.UI.WPF.ViewModels;
+
+public class MainViewModel : ViewModelBase {
+
+    private readonly INotificationService _notificationService;
+    private readonly NavigationStore _navigationStore;
+    public ViewModelBase? CurrentViewModel => _navigationStore.CurrentViewModel;
+
+    private string _message = "";
+    public string Message { get => _message; set => SetAndRaise(nameof(Message), ref _message, value); }
+
+    private bool _messageBoxVisibility = false;
+    public bool MessageBoxVisibility { get => _messageBoxVisibility; set => SetAndRaise(nameof(MessageBoxVisibility), ref _messageBoxVisibility, value); }
+
+    public ICommand MinimizeCommand { get; }
+    public ICommand ResizeCommand { get; }
+    public ICommand CloseCommand { get; }
+
+    public ICommand CloseMessageCommand { get; }
+
+    public MainViewModel(NavigationStore navigationStore, Window mainWindow, INotificationService notificationService) {
+        _navigationStore = navigationStore;
+        _notificationService = notificationService;
+        MinimizeCommand = new MinimizeCommand(mainWindow);
+        ResizeCommand = new ResizeCommand(mainWindow);
+        CloseCommand = new CloseCommand(mainWindow);
+        CloseMessageCommand = new RelayCommand(CloseMessage);
+
+        _navigationStore.CurrentViewModelChanged += OnCurrentViewChanged;
+        _notificationService.NewMessage += OnNewMessage;
+    }
+
+    private void OnNewMessage(string message) {
+        Message = message;
+        MessageBoxVisibility = true;
+    }
+
+    private void CloseMessage() {
+        Message = "";
+        MessageBoxVisibility = false;
+    }
+
+    private void OnCurrentViewChanged() {
+        OnPropertyChanged(nameof(CurrentViewModel));
+    }
+
+    protected override void Dispose(bool disposed) {
+        _notificationService.NewMessage -= OnNewMessage;
+        _navigationStore.CurrentViewModelChanged -= OnCurrentViewChanged;
+        base.Dispose(disposed);
+    }
+
+}
