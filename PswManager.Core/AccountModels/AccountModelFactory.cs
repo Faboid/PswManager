dfabf34e9555a8bcd6334a46dfe895d83f93d4ca
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-24 16:37:27 +0200

    Added documentation to interface.

--- PswManager.Core/AccountModels/AccountModelFactory.cs
+++ PswManager.Core/AccountModels/AccountModelFactory.cs
@@ -9,24 +9,10 @@ public class AccountModelFactory : IAccountModelFactory {
 		_cryptoAccountService = cryptoAccountService;
 	}
 
-	/// <summary>
-	/// Takes in encrypted values to create an encrypted account model.
-	/// </summary>
-	/// <param name="name"></param>
-	/// <param name="password"></param>
-	/// <param name="email"></param>
-	/// <returns></returns>
 	public IAccountModel CreateEncryptedAccount(string name, string password, string email) {
 		return new EncryptedAccount(name, password, email, _cryptoAccountService);
 	}
 
-	/// <summary>
-	/// Takes in plain-text values to create a decrypted account model.
-	/// </summary>
-	/// <param name="name"></param>
-	/// <param name="password"></param>
-	/// <param name="email"></param>
-	/// <returns></returns>
 	public IAccountModel CreateDecryptedAccount(string name, string password, string email) {
 		return new DecryptedAccount(name, password, email, _cryptoAccountService);
 	}
--- PswManager.Core/AccountModels/IAccountModelFactory.cs
+++ PswManager.Core/AccountModels/IAccountModelFactory.cs
@@ -1,6 +1,25 @@
 ﻿namespace PswManager.Core.AccountModels;
 
+/// <summary>
+/// Provides methods to instance <see cref="IAccountModel"/>.
+/// </summary>
 public interface IAccountModelFactory {
+
+    /// <summary>
+    /// Takes in encrypted values to create an encrypted account model.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="password"></param>
+    /// <param name="email"></param>
+    /// <returns></returns>
     IAccountModel CreateDecryptedAccount(string name, string password, string email);
+
+    /// <summary>
+    /// Takes in plain-text values to create a decrypted account model.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="password"></param>
+    /// <param name="email"></param>
+    /// <returns></returns>
     IAccountModel CreateEncryptedAccount(string name, string password, string email);
 }
