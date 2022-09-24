886965d28709015ef59efd4f370ac51d1b9260a8
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-24 11:00:18 +0200

    Implemented centralized AccountValidator

---
+++ PswManager.Core/Validators/IAccountValidator.cs
@@ -0,0 +1,37 @@
+﻿using PswManager.Database.Models;
+
+namespace PswManager.Core.Validators;
+public interface IAccountValidator {
+
+    public NameValid IsNameValid(AccountModel account);
+    public PasswordValid IsPasswordValid(AccountModel account);
+    public EmailValid IsEmailValid(AccountModel account);
+    public AccountValid IsAccountValid(AccountModel account);
+
+}
+
+public enum AccountValid {
+    Unknown,
+    Valid,
+    NameEmptyOrNull,
+    PasswordEmptyOrNull,
+    EmailEmptyOrNull,
+}
+
+public enum NameValid {
+    Unknown,
+    Valid,
+    EmptyOrNull
+}
+
+public enum PasswordValid {
+    Unknown,
+    Valid,
+    EmptyOrNull
+}
+
+public enum EmailValid {
+    Unknown,
+    Valid,
+    EmptyOrNull
+}
