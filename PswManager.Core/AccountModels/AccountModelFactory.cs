7629aadd9a7a1c55185566d43317e1d0edad5e01
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-24 16:16:46 +0200

    Implemented a system to distinguish between encrypted and plain-text models.

---
+++ PswManager.Core/AccountModels/AccountModelFactory.cs
@@ -0,0 +1,34 @@
+﻿using PswManager.Core.Services;
+namespace PswManager.Core.AccountModels;
+
+public class AccountModelFactory {
+
+	private readonly ICryptoAccountService _cryptoAccountService;
+
+	public AccountModelFactory(ICryptoAccountService cryptoAccountService) {
+		_cryptoAccountService = cryptoAccountService;
+	}
+
+	/// <summary>
+	/// Takes in encrypted values to create an encrypted account model.
+	/// </summary>
+	/// <param name="name"></param>
+	/// <param name="password"></param>
+	/// <param name="email"></param>
+	/// <returns></returns>
+	public IAccountModel CreateEncryptedAccount(string name, string password, string email) {
+		return new EncryptedAccount(name, password, email, _cryptoAccountService);
+	}
+
+	/// <summary>
+	/// Takes in plain-text values to create a decrypted account model.
+	/// </summary>
+	/// <param name="name"></param>
+	/// <param name="password"></param>
+	/// <param name="email"></param>
+	/// <returns></returns>
+	public IAccountModel CreateDecryptedAccount(string name, string password, string email) {
+		return new DecryptedAccount(name, password, email, _cryptoAccountService);
+	}
+
+}
---
+++ PswManager.Core/AccountModels/DecryptedAccount.cs
@@ -0,0 +1,29 @@
+﻿using PswManager.Core.Services;
+using System.Threading.Tasks;
+
+namespace PswManager.Core.AccountModels;
+
+public class DecryptedAccount : IAccountModel {
+
+	private readonly ICryptoAccountService _cryptoAccountService;
+
+	public DecryptedAccount(string name, string password, string email, ICryptoAccountService cryptoAccountService) {
+		_cryptoAccountService = cryptoAccountService;
+		Name = name;
+		Password = password;
+		Email = email;
+	}
+
+	public string Name { get; private set; }
+	public string Password { get; private set; }
+	public string Email { get; private set; }
+
+	public bool IsEncypted => false;
+	public bool IsPlainText => true;
+
+	public DecryptedAccount GetDecryptedAccount() => this;
+	public Task<DecryptedAccount> GetDecryptedAccountAsync() => Task.FromResult(this);
+	public EncryptedAccount GetEncryptedAccount()
+		=> new(Name, _cryptoAccountService.GetPassCryptoService().Encrypt(Password), _cryptoAccountService.GetEmaCryptoService().Encrypt(Email), _cryptoAccountService);
+	public Task<EncryptedAccount> GetEncryptedAccountAsync() => Task.Run(GetEncryptedAccount);
+}
