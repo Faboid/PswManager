0ee42c6882d125f764db59fd33040cb2eae60197
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-24 16:58:58 +0200

    Extracted AccountFactory.

---
+++ PswManager.Core/IAccountFactory.cs
@@ -0,0 +1,10 @@
+﻿using PswManager.Database.Models;
+using PswManager.Utils;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PswManager.Core;
+public interface IAccountFactory {
+    Task<Option<IAccount, AccountFactory.CreateAccountErrorCode>> CreateAccountAsync(AccountModel model);
+    Task<Option<IAsyncEnumerable<IAccount>>> LoadAccounts();
+}
