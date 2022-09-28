05657335abb825b169c070538f42ed6a54d912d6
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-28 16:20:17 +0200

    Implemented BindablePasswordBox, a PasswordBox that can bind the Password property

---
+++ PswManager.UI.WPF/Components/BindablePasswordBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PswManager.UI.WPF.Components;
+/// <summary>
+/// Interaction logic for BindablePasswordBox.xaml
+/// </summary>
+public partial class BindablePasswordBox : UserControl {
+
+    public string Password {
+        get { return (string)GetValue(PasswordProperty); }
+        set { SetValue(PasswordProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Password.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty PasswordProperty =
+        DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox), new PropertyMetadata(string.Empty, PasswordChangedFromSource));
+
+    private static void PasswordChangedFromSource(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        if(d is BindablePasswordBox box) {
+            box.UpdatePassword();
+        }
+    }
+
+    public BindablePasswordBox() {
+        InitializeComponent();
+    }
+
+    private bool _isPasswordChanging = false;
+
+    private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e) {
+        _isPasswordChanging = true;
+        Password = _passwordBox.Password;
+        _isPasswordChanging = false;
+    }
+
+    private void UpdatePassword() {
+        if(!_isPasswordChanging) {
+            _passwordBox.Password = Password;
+        }
+    }
+}
