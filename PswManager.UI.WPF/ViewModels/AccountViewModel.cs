02d7617e234fd158f446ce6a6e6b72ca9dfd19eb
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-30 15:47:10 +0200

    Implemented basic fields & constructor.

--- PswManager.UI.WPF/ViewModels/AccountViewModel.cs
+++ PswManager.UI.WPF/ViewModels/AccountViewModel.cs
@@ -1,7 +1,17 @@
+using PswManager.Core;
+
 namespace PswManager.UI.WPF.ViewModels;
 
 public class AccountViewModel : ViewModelBase {
 
+    private readonly IAccount _account;
 
+    public string Name => _account.Name;
+    public string EncryptedPassword => _account.Password;
+    public string EncryptedEmail => _account.Email;
+
+    public AccountViewModel(IAccount account) {
+        _account = account;
+    }
 
 }
