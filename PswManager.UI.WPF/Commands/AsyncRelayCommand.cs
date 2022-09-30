55f5bd607e4d9af87cda8c3f5c402633fd5c1c4d
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-30 16:56:01 +0200

    Imported async relay command

---
+++ PswManager.UI.WPF/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using PswManager.UI.WPF.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace PswManager.UI.WPF.Commands;
+
+public class AsyncRelayCommand : AsyncCommandBase {
+
+    private readonly Func<Task> _callback;
+
+    public AsyncRelayCommand(Func<Task> callback) {
+        _callback = callback;
+    }
+
+    public AsyncRelayCommand(Func<Task> callback, BusyService busyService) : base(busyService) {
+        _callback = callback;
+    }
+
+    protected override Task ExecuteAsync(object? parameter) => _callback.Invoke();
+
+}
+
+public class AsyncRelayCommand<T> : AsyncCommandBase {
+
+    private readonly Func<T, Task> _callback;
+
+    public AsyncRelayCommand(Func<T, Task> callback) {
+        _callback = callback;
+    }
+
+    public AsyncRelayCommand(Func<T, Task> callback, BusyService busyService) : base(busyService) {
+        _callback = callback;
+    }
+
+    public override bool CanExecute(object? parameter) {
+        return (parameter == null || parameter is T) && base.CanExecute(parameter);
+    }
+
+    protected override Task ExecuteAsync(object? parameter) => _callback?.Invoke((T)parameter!)!;
+}
