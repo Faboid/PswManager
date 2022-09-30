e434b705ab42b556f7d12aad1f80ef87ecba3280
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-30 15:46:36 +0200

    Imported navigate command

---
+++ PswManager.UI.WPF/Commands/NavigateCommand.cs
@@ -0,0 +1,24 @@
+﻿using PswManager.UI.WPF.Services;
+using PswManager.UI.WPF.ViewModels;
+
+namespace PswManager.UI.WPF.Commands;
+
+public class NavigateCommand<T> : LinkableCommandBase where T : ViewModelBase {
+
+    private readonly NavigationService<T> _navigationService;
+    private readonly bool _disposeCurrent;
+
+    public NavigateCommand(bool disposeCurrent, NavigationService<T> navigationService) {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+    }
+
+    public NavigateCommand(bool disposeCurrent, NavigationService<T> navigationService, BusyService busyService) : base(busyService) {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+    }
+
+    public override void ExecuteLinked(object? parameter) {
+        _navigationService.Navigate(_disposeCurrent);
+    }
+}
