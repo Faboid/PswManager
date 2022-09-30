818fb63a59ad4774358349019d6e59db12cba6ac
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-30 10:45:23 +0200

    Added class documentation.

--- PswManager.UI.WPF/ConstantValues/Debugging.cs
+++ PswManager.UI.WPF/ConstantValues/Debugging.cs
@@ -1,5 +1,9 @@
 namespace PswManager.UI.WPF.ConstantValues;
 #if DEBUG
+
+/// <summary>
+/// This is a debug-only class that provides constant values for an easier debugging. It does not exist in Release.
+/// </summary>
 public static class Debugging {
 
     public const string MasterKey = "ThisisAValidPassword, Only to be used for debugging";
