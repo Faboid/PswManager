4e2ede7a476c2d508902b146b70cd039e6162fa7
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-04 16:03:41 +0200

    Implemented a mock IPathsBuilder to use on tests.

---
+++ PswManager.Database.Tests/Mocks/MockPathsBuilder.cs
@@ -0,0 +1,28 @@
+using Moq;
+using PswManager.Paths;
+using System.IO.Abstractions.TestingHelpers;
+
+namespace PswManager.Database.Tests.Mocks;
+
+public class MockPathsBuilder : IPathsBuilder {
+
+    public MockPathsBuilder() {
+        _guid = Guid.NewGuid().ToString();
+    }
+
+    public MockPathsBuilder(string name) {
+        _guid = $"{Guid.NewGuid()}{name}";
+    }
+
+    private readonly string _guid;
+    private readonly PathsBuilder _pathsBuilder = new(new MockFileSystem().DirectoryInfo);
+
+    public string GetDataDirectory() => Path.Combine(GetWorkingDirectory(), "TestData");
+    public string GetJsonDatabaseDirectory() => Path.Combine(GetDatabaseDirectory(), "Json", _guid);
+    public string GetSQLDatabaseDirectory() => Path.Combine(GetDatabaseDirectory(), "SQLDB", _guid);
+    public string GetSQLDatabaseFile() => Path.Combine(GetSQLDatabaseDirectory(), "DB.db");
+    public string GetTextDatabaseDirectory() => Path.Combine(GetDatabaseDirectory(), "TextDB", _guid);
+    public string GetDatabaseDirectory() => Path.Combine(_pathsBuilder.GetDataDirectory(), "Saves", _guid);
+    public string GetWorkingDirectory() => _pathsBuilder.GetWorkingDirectory();
+
+}
