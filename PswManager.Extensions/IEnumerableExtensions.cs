fabd78067c3500ee77b5bb366a97019342281ce0
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-05 18:12:24 +0200

    Added documentation for the PswManager.Extensions assembly.

--- PswManager.Extensions/IEnumerableExtensions.cs
+++ PswManager.Extensions/IEnumerableExtensions.cs
@@ -1,6 +1,13 @@
 ﻿namespace PswManager.Extensions;
 public static class IEnumerableExtensions {
 
+    /// <summary>
+    /// Enumerates the given enumerable, executes an action for each item, and returns the enumerable.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumeration"></param>
+    /// <param name="action"></param>
+    /// <returns></returns>
     public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumeration, Action<T> action) {
         foreach(T item in enumeration) {
             action.Invoke(item);
@@ -8,14 +15,32 @@ public static class IEnumerableExtensions {
         return enumeration;
     }
 
+    /// <summary>
+    /// <inheritdoc cref="string.Join{T}(char, IEnumerable{T})"/>
+    /// </summary>
+    /// <param name="enumeration"></param>
+    /// <param name="separator"></param>
+    /// <returns></returns>
     public static string JoinStrings(this IEnumerable<string> enumeration, char separator) {
         return string.Join(separator, enumeration);
     }
 
+    /// <summary>
+    /// <inheritdoc cref="string.Join(string?, IEnumerable{string?})"/>
+    /// </summary>
+    /// <param name="enumeration"></param>
+    /// <param name="separator"></param>
+    /// <returns></returns>
     public static string JoinStrings(this IEnumerable<string> enumeration, string separator) {
         return string.Join(separator, enumeration);
     }
 
+    /// <summary>
+    /// Projects the <see cref="IEnumerable{T}"/> into an <see cref="IAsyncEnumerable{T}"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <returns></returns>
     public static async IAsyncEnumerable<T> AsAsyncEnumerable<T>(this IEnumerable<Task<T>> enumerable) {
         foreach(var value in enumerable) {
             yield return await value.ConfigureAwait(false);
