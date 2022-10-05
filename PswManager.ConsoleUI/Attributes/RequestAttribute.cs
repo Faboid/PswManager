81002eab81c7e9b55d2a0d328a160c8be22fe201
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-05 12:40:23 +0200

    Implemented documentation for the RequestAttribute.

--- PswManager.ConsoleUI/Attributes/RequestAttribute.cs
+++ PswManager.ConsoleUI/Attributes/RequestAttribute.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 
 namespace PswManager.ConsoleUI.Attributes;
+
+/// <summary>
+/// Requests <see cref="Requester"/> to ask for property to the console.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 public class RequestAttribute : Attribute {
 
+    /// <summary>
+    /// The name of the property.
+    /// </summary>
     public string DisplayName { get; private set; }
+
+    /// <summary>
+    /// The message used to request it to the user.
+    /// </summary>
     public string RequestMessage { get; private set; }
+
+    /// <summary>
+    /// Whether the property can be left empty.
+    /// </summary>
     public bool Optional { get; private set; }
 
     public RequestAttribute(string displayName, string requestMessage) {
