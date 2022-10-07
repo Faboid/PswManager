1af7a21ad6355067bca47189ee2873d319d4b978
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-07 14:34:00 +0200

    Implemented unhandled exception logger.

--- PswManager.UI.WPF/App.xaml.cs
+++ PswManager.UI.WPF/App.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using PswManager.Core.Services;
 using PswManager.Paths;
 using PswManager.UI.WPF.HostBuilders;
 using PswManager.UI.WPF.Stores;
 using PswManager.UI.WPF.ViewModels;
 using Serilog;
+using System;
 using System.IO;
 using System.Windows;
 
@@ -33,13 +35,20 @@ public partial class App : Application {
 			.AddViewModels()
 			.AddMainWindow()
 			.Build();
+
+		AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionsLogger;
+	}
+
+	private void UnhandledExceptionsLogger(object sender, UnhandledExceptionEventArgs e) {
+		var ex = (Exception)e.ExceptionObject;
+		var logger = _host.Services.GetRequiredService<ILogger<App>>();
+		logger.LogCritical(ex, "There has been an unhandled exception.");
 	}
 
 	protected override void OnStartup(StartupEventArgs e) {
 		_host.Start();
 
 		var tokenService = _host.Services.GetRequiredService<ITokenService>();
-
 		ViewModelBase startingVm;
 		if(tokenService.IsSet()) {
 			startingVm = _host.Services.GetRequiredService<LoginViewModel>();
