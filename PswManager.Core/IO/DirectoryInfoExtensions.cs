845a36602d0288445897fadd665b2446d967fd7a
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-21 18:14:39 +0200

    Imported extension methods to copy directories and files asynchonously.

---
+++ PswManager.Core/IO/DirectoryInfoExtensions.cs
@@ -0,0 +1,56 @@
+﻿using PswManager.Extensions;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PswManager.Core.IO;
+
+public static class DirectoryInfoExtensions {
+
+    /// <summary>
+    /// Copies to another directory. All subdirectories and files will be copied to it with their relative paths.
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="path"></param>
+    public static void CopyTo(this IDirectoryInfo info, string path) {
+        info.FileSystem.Directory.CreateDirectory(path);
+
+        //create all directories
+        info
+            .EnumerateDirectories("*", SearchOption.AllDirectories)
+            .Select(x => x.FullName.Replace(info.FullName, path))
+            .ForEach(x =>  info.FileSystem.Directory.CreateDirectory(x));
+
+        //copy all files
+        info
+            .EnumerateFiles("*", SearchOption.AllDirectories)
+            .Select(x => (File: x, NewPath: x.FullName.Replace(info.FullName, path)))
+            .ForEach(x => x.File.CopyTo(x.NewPath));
+    }
+
+    /// <summary>
+    /// Copies to another directory asynchronously. All subdirectories and files will be copied to it with their relative paths.
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static async Task CopyToAsync(this IDirectoryInfo info, string path) {
+        info.FileSystem.Directory.CreateDirectory(path);
+
+        //create all directories
+        info
+            .EnumerateDirectories("*", SearchOption.AllDirectories)
+            .Select(x => x.FullName.Replace(info.FullName, path))
+            .ForEach(x => info.FileSystem.Directory.CreateDirectory(x));
+
+        //copy all files
+        var tasks = info.EnumerateFiles("*", SearchOption.AllDirectories)
+            .Select(x => (File: x, NewFile: x.FullName.Replace(info.FullName, path)))
+            .Select(x => x.File.CopyToAsync(x.NewFile))
+            .ToArray();
+
+        await Task.WhenAll(tasks);
+    }
+
+}
---
+++ PswManager.Core/IO/FileInfoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Abstractions;
+using System.Threading.Tasks;
+
+namespace PswManager.Core.IO;
+
+public static class FileInfoExtensions {
+
+    /// <summary>
+    /// Copies an existing file to a new file asynchronously.
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static async Task CopyToAsync(this IFileInfo info, string path) {
+        using var reader = info.OpenRead();
+        using var writer = info.FileSystem.File.Create(path);
+        await reader.CopyToAsync(writer);
+    }
+
+}
