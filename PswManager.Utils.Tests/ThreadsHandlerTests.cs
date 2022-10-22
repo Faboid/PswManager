2e4ff761e5f5617810a894ebc577eb52227fc7c1
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-22 11:56:23 +0200

    Imported ThreadsHandler.

---
+++ PswManager.Utils.Tests/ThreadsHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace PswManager.Utils.Tests;
+
+public class ThreadsHandlerTests {
+
+    [Fact]
+    public void ThreadsHandlerSetsCurrentToForeground() {
+
+        var starting = Thread.CurrentThread.IsBackground;
+        Thread.CurrentThread.IsBackground = true;
+
+        var handler = ThreadsHandler.SetScopedForeground();
+        var updated = Thread.CurrentThread.IsBackground;
+        handler.Dispose();
+        var afterDisposal = Thread.CurrentThread.IsBackground;
+
+        //assert
+        Assert.False(updated);
+        Assert.True(afterDisposal);
+        Thread.CurrentThread.IsBackground = starting;
+
+    }
+
+}
