dc99afdeeac18d67a1f2e275c13e54f5faad2f08
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-22 17:55:12 +0200

    Implemented factory for ICryptoService.

---
+++ PswManager.Encryption/Services/CryptoServiceFactory.cs
@@ -0,0 +1,11 @@
+﻿using PswManager.Encryption.Cryptography;
+namespace PswManager.Encryption.Services;
+
+public class CryptoServiceFactory : ICryptoServiceFactory {
+    
+    public ICryptoService GetCryptoService(char[] password) => new CryptoService(password);
+    public ICryptoService GetCryptoService(char[] password, string version) => new CryptoService(password, version);
+    public ICryptoService GetCryptoService(Key key) => new CryptoService(key);
+    public ICryptoService GetCryptoService(Key key, string version) => new CryptoService(key, version);
+
+}
---
+++ PswManager.Encryption/Services/ICryptoServiceFactory.cs
@@ -0,0 +1,11 @@
+﻿using PswManager.Encryption.Cryptography;
+namespace PswManager.Encryption.Services;
+
+public interface ICryptoServiceFactory {
+
+    ICryptoService GetCryptoService(char[] password);
+    ICryptoService GetCryptoService(char[] password, string version);
+    ICryptoService GetCryptoService(Key key);
+    ICryptoService GetCryptoService(Key key, string version);
+
+}
