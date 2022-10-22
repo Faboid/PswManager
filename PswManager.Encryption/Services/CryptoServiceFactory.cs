18471bb285eecbd4171d5bf3eb7838ffccb19cad
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-22 18:19:15 +0200

    Set internal methods through an internal interface.

--- PswManager.Encryption/Services/CryptoServiceFactory.cs
+++ PswManager.Encryption/Services/CryptoServiceFactory.cs
@@ -1,11 +1,12 @@
 ﻿using PswManager.Encryption.Cryptography;
+using System;
+
 namespace PswManager.Encryption.Services;
 
-public class CryptoServiceFactory : ICryptoServiceFactory {
+public class CryptoServiceFactory : ICryptoServiceInternalFactory {
     
     public ICryptoService GetCryptoService(char[] password) => new CryptoService(password);
-    public ICryptoService GetCryptoService(char[] password, string version) => new CryptoService(password, version);
     public ICryptoService GetCryptoService(Key key) => new CryptoService(key);
-    public ICryptoService GetCryptoService(Key key, string version) => new CryptoService(key, version);
-
+    ICryptoService ICryptoServiceInternalFactory.GetCryptoService(char[] password, string version) => new CryptoService(password, version);
+    ICryptoService ICryptoServiceInternalFactory.GetCryptoService(Key key, string version) => new CryptoService(key, version);
 }
--- PswManager.Encryption/Services/ICryptoServiceFactory.cs
+++ PswManager.Encryption/Services/ICryptoServiceFactory.cs
@@ -4,8 +4,13 @@ namespace PswManager.Encryption.Services;
 public interface ICryptoServiceFactory {
 
     ICryptoService GetCryptoService(char[] password);
-    ICryptoService GetCryptoService(char[] password, string version);
     ICryptoService GetCryptoService(Key key);
+
+}
+
+internal interface ICryptoServiceInternalFactory : ICryptoServiceFactory {
+ 
+    ICryptoService GetCryptoService(char[] password, string version);
     ICryptoService GetCryptoService(Key key, string version);
 
 }
