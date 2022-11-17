3ba53b17ab0b9969a1d88c4194fd1a3f3ba8848f
Faboid <72757782+Faboid@users.noreply.github.com>
2022-11-17 18:09:12 +0100

    Set up second password-repeat field.

--- PswManager.UI.WPF/ViewModels/SettingsViewModel.cs
+++ PswManager.UI.WPF/ViewModels/SettingsViewModel.cs
@@ -4,6 +4,9 @@ using PswManager.Core.Services;
 using PswManager.UI.WPF.Commands;
 using PswManager.UI.WPF.Services;
 using PswManager.UI.WPF.Stores;
+using System;
+using System.Collections;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace PswManager.UI.WPF.ViewModels;
@@ -19,7 +22,19 @@ public class SettingsViewModel : ViewModelBase {
     private string _newPassword = "";
     public string NewPassword {
         get { return _newPassword; }
-        set { SetAndRaise(nameof(NewPassword), ref _newPassword, value); }
+        set { 
+            SetAndRaise(nameof(NewPassword), ref _newPassword, value);
+            OnPropertyChanged(nameof(CanChangePassword));
+        }
+    }
+
+    private string _repeatNewPassword = "";
+    public string RepeatNewPassword {
+        get { return _repeatNewPassword; }
+        set { 
+            SetAndRaise(nameof(RepeatNewPassword), ref _repeatNewPassword, value);
+            OnPropertyChanged(nameof(CanChangePassword));
+        }
     }
 
     public SettingsViewModel(NavigationService<AccountsListingViewModel> navigationServiceToListingViewModel, 
@@ -33,5 +48,7 @@ public class SettingsViewModel : ViewModelBase {
         HomeButton = new NavigateCommand<AccountsListingViewModel>(true, navigationServiceToListingViewModel, busyService);
         ChangePasswordCommand = new ChangePasswordCommand(this, accountsStore, passwordEditor, cryptoContainerService, notificationService, cryptoAccountServiceFactory, busyService, loggerFactory);
     }
+
+    public bool CanChangePassword => !string.IsNullOrWhiteSpace(NewPassword) && NewPassword == RepeatNewPassword;
 
 }
